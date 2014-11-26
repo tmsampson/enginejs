99163276b8be6fa759f279d7ef72803184714e35
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2014-11-26 11:15:50 +0000

    > Implemented Engine2D_Entity > Engine2D_Entity instances can now be added to scene for update/render

--- enginejs-2d.js
+++ enginejs-2d.js
@@ -1,6 +1,7 @@
 function Engine2D_Scene(engine)
 {
 	this.engine = engine;
+	this.entities = [];
 
 	// Setup the viewport to match the canvas by default
 	var viewport_size =
@@ -14,8 +15,20 @@ function Engine2D_Scene(engine)
 	this.background = new Engine2D_Background(engine);
 
 	// Setup shader programs
-	this.program_grid  = engine.CreateShaderProgram(Engine.Resources["vs_basic_transformed"],
-	                                                Engine.Resources["fs_grid"]);
+	this.program_grid    = engine.CreateShaderProgram(Engine.Resources["vs_basic_transformed"],
+	                                                  Engine.Resources["fs_grid"]);
+	this.program_sprite  = engine.CreateShaderProgram(Engine.Resources["vs_basic_transformed"],
+	                                                  Engine.Resources["fs_2d_sprite"]);
+}
+
+Engine2D_Scene.prototype.Add = function(entity)
+{
+	this.entities.push(entity);
+}
+
+Engine2D_Scene.prototype.Clear = function()
+{
+	this.entities = [];
 }
 
 Engine2D_Scene.prototype.Render = function(info)
@@ -23,6 +36,7 @@ Engine2D_Scene.prototype.Render = function(info)
 	// Update & bind camera
 	this.camera.Update(info);
 	this.engine.BindCamera(this.camera);
+	var mtx_trans = mat4.create();
 
 	// Render background (or grid if background not setup)
 	var background_in_use = this.background.hasOwnProperty("colour") ||
@@ -33,10 +47,42 @@ Engine2D_Scene.prototype.Render = function(info)
 	}
 	else
 	{
-		var mtx_trans = mat4.create();
 		mat4.scale(mtx_trans, mtx_trans, [this.engine.GetCanvasWidth(), this.engine.GetCanvasHeight(), 0.0]);
 		this.engine.BindShaderProgram(this.program_grid);
 		this.engine.SetShaderConstant("u_trans_model", mtx_trans, Engine.SC_MATRIX4);
+		this.engine.DrawQuad();
+	}
+
+	// Update entities
+	for(var i = 0; i < this.entities.length; ++i)
+	{
+		var entity = this.entities[i];
+
+		// Integrate linear velocity
+		entity.position[0] += entity.velocity[0];
+		entity.position[1] += entity.velocity[1];
+	}
+
+	// Render entities
+	this.entities.sort(function(a,b){ return a.depth >= b.depth; });
+	this.engine.SetBlendMode(this.engine.gl.SRC_ALPHA, this.engine.gl.ONE_MINUS_SRC_ALPHA, true);
+	for(var i = 0; i < this.entities.length; ++i)
+	{
+		var entity = this.entities[i];
+
+		// Setup transforms
+		var entity_trans = [entity.position[0], entity.position[1], entity.depth];
+		var entity_scale = [entity.size[0] / 2, entity.size[1] / 2, 0.0]; // half size as DrawQuad is 2x2 clip space
+
+		mat4.translate(mtx_trans, Engine.IdentityMatrix, entity_trans);
+		mat4.rotate(mtx_trans, mtx_trans, entity.rotation, [0, 0, 1]);
+		mat4.scale(mtx_trans, mtx_trans, entity_scale);
+
+		// Draw
+		this.engine.BindShaderProgram(this.program_sprite);
+		this.engine.SetShaderConstant("u_tint", entity.tint, Engine.SC_VEC4);
+		this.engine.SetShaderConstant("u_trans_model", mtx_trans, Engine.SC_MATRIX4);
+		this.engine.BindTexture(entity.texture, 0);
 		this.engine.DrawQuad();
 	}
 }
@@ -92,4 +138,51 @@ Engine2D_Background.prototype.Render = function(info)
 	{
 		this.engine.Clear(this.colour);
 	}
+}
+
+function Engine2D_Entity(texture)
+{
+	this.texture = texture;
+	this.size = [texture.width, texture.height];
+	this.position = [0, 0];
+	this.velocity = [0, 0];
+	this.rotation = 0;
+	this.depth = 0; // 0 = front
+	this.tint = [1, 1, 1, 1];
+	this.alpha = 1;
+}
+
+Engine2D_Entity.prototype.SetVelocity = function(x, y)
+{
+	this.velocity[0] = x;
+	this.velocity[1] = y;
+}
+
+Engine2D_Entity.prototype.MoveTo = function(x, y)
+{
+	this.position[0] = x;
+	this.position[1] = y;
+}
+
+Engine2D_Entity.prototype.SetRotation = function(theta)
+{
+	this.rotation = theta;
+}
+
+Engine2D_Entity.prototype.SetSize = function(size)
+{
+	this.size[0] = size;
+	this.size[1] = size;
+}
+
+Engine2D_Entity.prototype.SetTint = function(r, g, b)
+{
+	this.tint[0] = r;
+	this.tint[1] = g;
+	this.tint[2] = b;
+}
+
+Engine2D_Entity.prototype.SetAlpha = function(alpha)
+{
+	this.tint[3] = alpha;
 }
