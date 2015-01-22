021dfa85db2feb687e1258403abb675678ba0ea4
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2015-01-22 17:44:52 +0000

    > Added Engine.Util.Clone for deep-cloning JS objects

--- modules/enginejs-util.js
+++ modules/enginejs-util.js
@@ -7,5 +7,12 @@ Engine.Util =
 	MD5 : function(data)
 	{
 		return HashCode.value(data);
+	},
+
+	Clone : function(obj)
+	{
+		var copiedObject = {};
+		jQuery.extend(copiedObject,obj);
+		return copiedObject;
 	}
 };
