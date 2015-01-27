ade18c8143ee6ada72387fd9061e1ad8da4216da
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2015-01-27 17:34:51 +0000

    > Overhauled Engine.Audio to support instanced sfx/bgm objects with own playback/volume controls

--- modules/enginejs-audio.js
+++ modules/enginejs-audio.js
@@ -20,56 +20,74 @@ Engine.Audio =
 		});
 	},
 
-	PlayBGM : function(sound_object, params)
+	Sound : function(sound_object, global_volume_node, loop)
 	{
-		Engine.Audio.PlaySound(sound_object, params, Engine.Audio.volume_nodes["bgm"]);
+		this.resource = sound_object;
+
+		// Setup source
+		this.source = Engine.Audio.context.createBufferSource();
+		this.source.buffer = sound_object.pcm_buffer;
+		this.source.loop = loop;
+
+		// Let this instance have it's own volume range, starting at
+		// full volume and clamped to global SFX volume
+		this.volume_node = Engine.Audio.context.createGain();
+		this.volume_node.gain.value = 1.0;
+		this.volume_node.connect(global_volume_node);
+		this.source.connect(this.volume_node);
+
+		// Playback control
+		this.Play = function() { this.source.start(0); };
+		this.Stop = function() { this.source.stop(0);  };
+
+		// Volume controls
+		this.GetVolume = function() { return this.volume_node.gain.value; };
+		this.SetVolume = function(volume) { this.volume_node.gain.value = volume; };
+
+		// Loop controls
+		this.IsLooped = function() { return this.source.loop; };
+		this.EnableLoop = function(loop) { this.source.loop = loop; };
 	},
 
-	PlaySFX : function(sound_object, params)
+	SoundEffect2D : function(sound_object)
 	{
-		Engine.Audio.PlaySound(sound_object, params, Engine.Audio.volume_nodes["sfx"]);
+		// Inherit base
+		$.extend(this, new Engine.Audio.Sound(sound_object, Engine.Audio.volume_nodes["sfx"], false));
 	},
 
-	PlaySound : function(sound_object, params, volume_node)
+	BackgroundMusic : function(sound_object)
 	{
-		var source = Engine.Audio.context.createBufferSource();
-		source.loop = (params && params["loop"])? params["loop"] : false;
-		source.buffer = sound_object.pcm_buffer;
-
-		// sound --> volume node --> speakers
-		source.connect(volume_node);
-		volume_node.connect(Engine.Audio.context.destination);
-		source.start(0);
+		// Inherit base
+		$.extend(this, new Engine.Audio.Sound(sound_object, Engine.Audio.volume_nodes["bgm"], true));
 	},
 
-	SetVolume : function(volume_node_name, volume)
+	SetGlobalBgmVolume : function(volume)
 	{
-		Engine.Audio.volume_nodes[volume_node_name].gain.value = volume;
+		Engine.Audio.volume_nodes["bgm"].gain.value = volume;
 	},
 
-	SetVolumeBGM : function(volume)
+	SetGlobalSfxVolume : function(volume)
 	{
-		Engine.Audio.SetVolume("bgm", volume);
-	},
-
-	SetVolumeSFX : function(volume)
-	{
-		Engine.Audio.SetVolume("sfx", volume);
+		Engine.Audio.volume_nodes["sfx"].gain.value = volume;
 	},
 };
-
-// *******************************************
-// Resource Load
-// *******************************************
-Engine.Resource.RegisterLoadFunction("mp3", Engine.Audio.LoadSound);
 
 // *******************************************
 // Init
 // *******************************************
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 Engine.Audio.context = new AudioContext();
+
+// Setup volume nodes
 Engine.Audio.volume_nodes =
 {
 	"bgm" : Engine.Audio.context.createGain(),
 	"sfx" : Engine.Audio.context.createGain()
 };
+
+// Connect volume nodes to audio output
+Engine.Audio.volume_nodes["bgm"].connect(Engine.Audio.context.destination);
+Engine.Audio.volume_nodes["sfx"].connect(Engine.Audio.context.destination);
+
+// Register handler for mp3 files
+Engine.Resource.RegisterLoadFunction("mp3", Engine.Audio.LoadSound);
