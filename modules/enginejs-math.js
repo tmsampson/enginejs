dbca2ca5e1ef320ebdb9e2796bd6cdeedfe2fbf4
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2015-02-22 19:33:41 +0000

    > Quick tidy up

--- modules/enginejs-math.js
+++ modules/enginejs-math.js
@@ -4,19 +4,22 @@
 
 Engine.Math =
 {
+	// Constants
+	IdentityMatrix : mat4.create(),
+
 	Clamp : function(x, min, max)
 	{
 		return Math.min(Math.max(x, min), max);
 	},
+
 	Random : function(min, max)
 	{
 		return Math.random() * (max - min) + min;
 	},
+
+
 	RandomInteger : function(min, max)
 	{
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	},
-
-	// Constants
-	IdentityMatrix : mat4.create(),
 };
