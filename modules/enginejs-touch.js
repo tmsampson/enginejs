4839336e278a5fed6ac3ab5f4dc0f49bef12440f
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2015-02-26 07:48:11 +0000

    > Added Engine.Touch press debounce

--- modules/enginejs-touch.js
+++ modules/enginejs-touch.js
@@ -6,10 +6,13 @@ Engine.Touch =
 {
 	streams : [],
 
-	IsPressed : function(index)
+	IsPressed : function(index, debounce)
 	{
 		var i = index || 0;
-		return Engine.Touch.streams.length > i;
+		var have_stream = Engine.Touch.streams.length > i;
+		if(!have_stream) { return false; }
+		return debounce? Engine.Array.GetLastValue(Engine.Touch.streams[i]).just_pressed :
+		                 true;
 	},
 
 	GetPosition : function(index)
@@ -17,6 +20,18 @@ Engine.Touch =
 		var i = index || 0;
 		if(!Engine.Touch.IsPressed(i)) { return null; }
 		return Engine.Array.GetLastValue(Engine.Touch.streams[i]).position;
+	},
+
+	Kick : function()
+	{
+		// Clear just_pressed flag where set
+		for(var i = 0; i < Engine.Touch.streams.length; ++i)
+		{
+			if(Engine.Touch.streams[i].length > 0)
+			{
+				Engine.Touch.streams[i][0].just_pressed = false;
+			}
+		}
 	},
 
 	// Internal event handling
@@ -27,7 +42,7 @@ Engine.Touch =
 		for(var i = 0; i < touches.length; ++i)
 		{
 			var touch = touches[i];
-			Engine.Touch._event_register(touch);
+			Engine.Touch._event_register(touch, true);
 		}
 	},
 
@@ -38,7 +53,7 @@ Engine.Touch =
 		for(var i = 0; i < touches.length; ++i)
 		{
 			var touch = touches[i];
-			Engine.Touch._event_register(touch);
+			Engine.Touch._event_register(touch, false);
 		}
 	},
 
@@ -61,14 +76,16 @@ Engine.Touch =
 
 	_event_register : function(touch)
 	{
+		Engine.Log("event");
 		if(Engine.Touch.streams.length == 2) { return; } // Only support 2 fingers
 
 		var touch_event =
 		{
-			identifier : touch.identifier,
-			time       : Engine.Time.Now(),
-			position   : [ touch.pageX - Engine.Canvas.getBoundingClientRect().left,
-			               Engine.Canvas.getBoundingClientRect().bottom - touch.pageY ],
+			identifier   : touch.identifier,
+			time         : Engine.Time.Now(),
+			position     : [ touch.pageX - Engine.Canvas.getBoundingClientRect().left,
+			                 Engine.Canvas.getBoundingClientRect().bottom - touch.pageY ],
+			just_pressed : false
 		};
 
 		// Find existing stream
@@ -89,6 +106,7 @@ Engine.Touch =
 		}
 		else
 		{
+			touch_event.just_pressed = true;
 			Engine.Touch.streams.push([touch_event]);
 		}
 	}
