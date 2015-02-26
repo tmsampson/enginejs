8780bff478fee1afaa612b8eb8839a652cc432c8
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2015-02-26 17:02:05 +0000

    > Added Engine.Canvas.GetMinDimension/GetMaxDimension

--- engine.js
+++ engine.js
@@ -143,12 +143,14 @@ Engine =
 			Engine.Canvas.is_fullscreen = false;
 
 			// Canvas helper methods
-			Engine.Canvas.GetSize   = function()      { return [this.width, this.height] };
-			Engine.Canvas.GetWidth  = function()      { return this.width; };
-			Engine.Canvas.GetHeight = function()      { return this.height; };
-			Engine.Canvas.GetCentre = function()      { return [this.width / 2, this.height / 2, 0] };
-			Engine.Canvas.GetCenter = function()      { return Engine.Canvas.GetCentre(); };
-			Engine.Canvas.GetAspectRatio = function() { return this.width / this.height; };
+			Engine.Canvas.GetSize   = function()       { return [this.width, this.height] };
+			Engine.Canvas.GetWidth  = function()       { return this.width; };
+			Engine.Canvas.GetHeight = function()       { return this.height; };
+			Engine.Canvas.GetCentre = function()       { return [this.width / 2, this.height / 2, 0] };
+			Engine.Canvas.GetCenter = function()       { return Engine.Canvas.GetCentre(); };
+			Engine.Canvas.GetAspectRatio = function()  { return this.width / this.height; };
+			Engine.Canvas.GetMaxDimension = function() { return this.width > this.height? this.width  : this.height; };
+			Engine.Canvas.GetMinDimension = function() { return this.width > this.height? this.height : this.width;  };
 			Engine.Canvas.EnableContextMenu = function(do_enable)
 			{
 				// Suppress canvas right-click context menu?
