4972d47a1ca2926c295b175fdd0e6226f1bdd97b
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2015-03-04 17:20:15 +0000

    > start_webserver.bat no longer blocks or clears stdout

--- utils/webserver/start_webserver.bat
+++ utils/webserver/start_webserver.bat
@@ -7,20 +7,12 @@ SET PORT=1234
 SET URI=http://%HOST%:%PORT%
 
 :: Kill previous instances
-taskkill /im tiny.exe /f /t
-CLS
+taskkill /im tiny.exe /f /t > nul
 
 :: Start webserver
 SET MY_ROOT=%~dp0
 START /D "%MY_ROOT%" tiny.exe %SERVER_ROOT% %PORT% > nul
-ECHO Webserver running @ %URI%
+ECHO * Webserver running @ %URI%
 
 :: Launch in browser
 START %URI%
-
-:: Terminate on request
-ECHO Press any key to terminate...
-PAUSE > nul
-taskkill /im tiny.exe /f /t
-popd
-CLS
