46b2d812d88d3de0b862356da942e31adbfcbd7e
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2015-03-09 13:52:56 +0000

    > Added Engine.Debug.DrawCircle > Implemented line thickness for Engine.Debug.DrawLine

--- modules/enginejs-debug.js
+++ modules/enginejs-debug.js
@@ -6,13 +6,16 @@ Engine.Debug =
 {
 	draw_commands : [],
 
-	// Shader programs
-	debug_draw_program_line : null,
+	// Resources
+	debug_draw_program_basic : null,
+	debug_draw_circle_model  : null,
 
 	PreGameLoopInit : function()
 	{
-		Engine.Debug.debug_draw_program_line = Engine.Gfx.CreateShaderProgram(Engine.Resources["vs_basic_transformed"],
+		Engine.Debug.debug_draw_program_basic = Engine.Gfx.CreateShaderProgram(Engine.Resources["vs_basic_transformed"],
 		                                       Engine.Resources["fs_basic_colour"]);
+
+		Engine.Debug.debug_draw_circle_model = Engine.Geometry.MakeCircle({ segment_count : 50 });
 	},
 
 	DrawLine : function(start, end, colour, thickness, camera)
@@ -31,6 +34,21 @@ Engine.Debug =
 		});
 	},
 
+	DrawCircle : function(position, radius, colour, camera)
+	{
+		draw_commands.push(
+		{
+			func   : Engine.Debug.debug_draw_func_circle,
+			config :
+			{
+				position : position,
+				radius   : radius,
+				colour   : colour || Engine.Colour.Red,
+				camera   : camera
+			}
+		});
+	},
+
 	Update : function()
 	{
 		// Note: This is called prior to any user rendering, so can be
@@ -43,6 +61,7 @@ Engine.Debug =
 		// Note: This is called post user-rendering so we can disable
 		//       z-test and render draw commands in order they were issued
 		Engine.Gfx.EnableDepthTest(false);
+		Engine.Gfx.EnableBlend(false);
 		for(var i = 0; i < draw_commands.length; ++i)
 		{
 			var command = draw_commands[i];
@@ -64,27 +83,70 @@ Engine.Debug =
 			                  config.end[0],   config.end[1],   0]
 		};
 
+		// Calculate line normal
+		var normal = Engine.Vec2.Normal(Engine.Vec2.Subtract(config.end, config.start));
+
 		// Bind shader
-		Engine.Gfx.BindShaderProgram(Engine.Debug.debug_draw_program_line);
+		Engine.Gfx.BindShaderProgram(Engine.Debug.debug_draw_program_basic);
 
 		// Create & bind line vertex stream
 		var line_vbo = Engine.Gfx.CreateVertexBuffer(line_verts);
 		Engine.Gfx.BindVertexBuffer(line_vbo);
 
 		// Setup transform (canvas space)
-		var mtx_identity = mat4.create(); mat4.identity(mtx_identity);
 		var mtx_proj = mat4.create(); mat4.identity(mtx_proj);
 		var canvas_size = Engine.Canvas.GetSize();
 		mat4.ortho(mtx_proj, 0, canvas_size[0], 0, canvas_size[1], -1, 1);
-		Engine.Gfx.SetShaderConstant("u_trans_model", mtx_identity, Engine.Gfx.SC_MATRIX4);
-		Engine.Gfx.SetShaderConstant("u_trans_view",  mtx_identity, Engine.Gfx.SC_MATRIX4);
-		Engine.Gfx.SetShaderConstant("u_trans_proj",  mtx_proj,     Engine.Gfx.SC_MATRIX4);
+		Engine.Gfx.SetShaderConstant("u_trans_model", Engine.Math.IdentityMatrix, Engine.Gfx.SC_MATRIX4);
+		Engine.Gfx.SetShaderConstant("u_trans_view",  Engine.Math.IdentityMatrix, Engine.Gfx.SC_MATRIX4);
+		Engine.Gfx.SetShaderConstant("u_trans_proj",  mtx_proj, Engine.Gfx.SC_MATRIX4);
 
 		// Set colour
 		Engine.Gfx.SetShaderConstant("u_colour", config.colour, Engine.Gfx.SC_COLOUR);
 
 		// Draw line
 		Engine.Gfx.DrawArray();
+
+		// Thicken line?
+		if(config.thickness > 1)
+		{
+			var mtx_trans = mat4.create();
+			var lines_per_side = Math.ceil((config.thickness - 1) / 2);
+			var lines_total = 1 + (lines_per_side * 2);
+			for(var i = 0; i < lines_per_side; ++i)
+			{
+				var shift = (config.thickness / lines_total) * (i + 1);
+				mat4.translate(mtx_trans, Engine.Math.IdentityMatrix, [normal[0] * shift, normal[1] * shift, 0]);
+				Engine.Gfx.SetShaderConstant("u_trans_model", mtx_trans, Engine.Gfx.SC_MATRIX4);
+				Engine.Gfx.DrawArray();
+				mat4.translate(mtx_trans, Engine.Math.IdentityMatrix, [-normal[0] * shift, -normal[1] * shift, 0]);
+				Engine.Gfx.SetShaderConstant("u_trans_model", mtx_trans, Engine.Gfx.SC_MATRIX4);
+				Engine.Gfx.DrawArray();
+			}
+		}
+	},
+
+	debug_draw_func_circle : function(config)
+	{
+		// Bind shader
+		Engine.Gfx.BindShaderProgram(Engine.Debug.debug_draw_program_basic);
+
+		// Setup transform (canvas space)
+		var mtx_trans = mat4.create();
+		mat4.translate(mtx_trans, Engine.Math.IdentityMatrix, [config.position[0], config.position[1], 0]);
+		mat4.scale(mtx_trans, mtx_trans, [config.radius, config.radius, 0.0]);
+		var mtx_proj = mat4.create(); mat4.identity(mtx_proj);
+		var canvas_size = Engine.Canvas.GetSize();
+		mat4.ortho(mtx_proj, 0, canvas_size[0], 0, canvas_size[1], -1, 1);
+		Engine.Gfx.SetShaderConstant("u_trans_model", mtx_trans, Engine.Gfx.SC_MATRIX4);
+		Engine.Gfx.SetShaderConstant("u_trans_view",  Engine.Math.IdentityMatrix, Engine.Gfx.SC_MATRIX4);
+		Engine.Gfx.SetShaderConstant("u_trans_proj",  mtx_proj, Engine.Gfx.SC_MATRIX4);
+
+		// Set colour
+		Engine.Gfx.SetShaderConstant("u_colour", config.colour, Engine.Gfx.SC_COLOUR);
+
+		// Draw circle
+		Engine.Gfx.DrawModel(Engine.Debug.debug_draw_circle_model);
 	},
 
 	Break : function()
