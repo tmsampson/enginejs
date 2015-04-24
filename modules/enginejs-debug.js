2854fc409cc9d6818adb0070a92500b956db4657
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2015-04-24 16:51:07 +0100

    > Engine.Gfx.DrawArray now allows caller to override draw_mode (useful when drawing sub-prims from shared polygon soup vbo) > Finished refactoring Engine.Debug draw functionality

--- modules/enginejs-debug.js
+++ modules/enginejs-debug.js
@@ -7,49 +7,79 @@ Engine.Debug =
 	draw_commands : [],
 
 	// Resources
-	debug_draw_program_basic : null,
-	debug_draw_circle_model  : null,
+	shader_program : null,
+	circle_model  : null,
 
-	// Vertex buffer objects
-	line_verts : { attribute_name : "a_pos", item_size : 3, draw_mode : "lines", stream : new Float32Array(30000), stream_index : 0 },
-	poly_verts : { attribute_name : "a_pos", item_size : 3, draw_mode : "triangle_fan", stream : new Float32Array(30000), stream_index : 0 },
+	// Dynamic polygon soup for debug geometry (arbitrary lines & polygons)
+	soup_vbo        : null,
+	soup_idx        : 0, // Current write offset into soup
+	soup_descriptor :
+	{
+		attribute_name : "a_pos",
+		item_size      : 3,
+		draw_mode      : "lines",
+		stream         : new Float32Array(30000),
+		stream_index  : 0
+	},
+
+	// Deferred queues for rect/cirlces
+	prim_queue : [],
 
 	PreGameLoopInit : function()
 	{
-		Engine.Debug.debug_draw_program_basic = Engine.Gfx.CreateShaderProgram(Engine.Resources["vs_basic_transformed_nouv"],
+		Engine.Debug.shader_program = Engine.Gfx.CreateShaderProgram(Engine.Resources["vs_basic_transformed_nouv"],
 		                                        Engine.Resources["fs_basic_colour"]);
+		Engine.Debug.circle_model = Engine.Geometry.MakeCircle({ segment_count : 50 });
 
-		Engine.Debug.debug_draw_circle_model = Engine.Geometry.MakeCircle({ segment_count : 50 });
-
-		// Setup vertex streams
-		Engine.Debug.line_verts.vbo = Engine.Gfx.CreateVertexBuffer(Engine.Debug.line_verts);
-		Engine.Debug.poly_verts.vbo = Engine.Gfx.CreateVertexBuffer(Engine.Debug.poly_verts);
+		// Setup vertex buffer object
+		Engine.Debug.soup_vbo = Engine.Gfx.CreateVertexBuffer(Engine.Debug.soup_descriptor);
 	},
 
-	RegisterDrawCommand : function(buffer, vertices, colour)
+	DeferDrawLine : function(vertices, colour)
 	{
 		// Register command
 		Engine.Debug.draw_commands.push(
 		{
-			buffer : buffer,
-			offset : buffer.stream_index / buffer.item_size,
-			count  : vertices.length,
-			colour : colour
+			offset    : Engine.Debug.soup_idx / Engine.Debug.soup_descriptor.item_size,
+			count     : vertices.length,
+			draw_mode : "lines",
+			colour    : colour
 		});
 
 		// Add verts to buffer
 		for(var i = 0; i < vertices.length; ++i)
 		{
-			buffer.stream[buffer.stream_index++] = vertices[i][0];
-			buffer.stream[buffer.stream_index++] = vertices[i][1];
-			buffer.stream[buffer.stream_index++] = 0;
+			Engine.Debug.soup_descriptor.stream[Engine.Debug.soup_idx++] = vertices[i][0];
+			Engine.Debug.soup_descriptor.stream[Engine.Debug.soup_idx++] = vertices[i][1];
+			Engine.Debug.soup_descriptor.stream[Engine.Debug.soup_idx++] = 0;
+		}
+	},
+
+	DeferDrawPoly : function(vertices, colour)
+	{
+		// Register command
+		Engine.Debug.draw_commands.push(
+		{
+			buffer    : Engine.Debug.soup_descriptor,
+			offset    : Engine.Debug.soup_idx / Engine.Debug.soup_descriptor.item_size,
+			count     : vertices.length,
+			draw_mode : "triangle_fan",
+			colour    : colour
+		});
+
+		// Add verts to buffer
+		for(var i = 0; i < vertices.length; ++i)
+		{
+			Engine.Debug.soup_descriptor.stream[Engine.Debug.soup_idx++] = vertices[i][0];
+			Engine.Debug.soup_descriptor.stream[Engine.Debug.soup_idx++] = vertices[i][1];
+			Engine.Debug.soup_descriptor.stream[Engine.Debug.soup_idx++] = 0;
 		}
 	},
 
 	DrawLine : function(start, end, colour, thickness, camera)
 	{
 		// Draw main line
-		Engine.Debug.RegisterDrawCommand(Engine.Debug.line_verts, [start, end], colour);
+		Engine.Debug.DeferDrawLine([start, end], colour);
 
 		// Thicken line?
 		if(thickness > 1)
@@ -61,72 +91,57 @@ Engine.Debug =
 			for(var i = 0; i < lines_per_side; ++i)
 			{
 				var shift = (thickness / lines_total) * (i + 1);
-				var extra_lines = [ Engine.Vec2.Add(start, [ normal[0]  * shift, normal[1] * shift]),   // Line A (start)
-				                    Engine.Vec2.Add(end,   [ normal[0]  * shift, normal[1] * shift]),   // Line A (end)
+				var extra_lines = [ Engine.Vec2.Add(start, [ normal[0] * shift,  normal[1] * shift]),   // Line A (start)
+				                    Engine.Vec2.Add(end,   [ normal[0] * shift,  normal[1] * shift]),   // Line A (end)
 				                    Engine.Vec2.Add(start, [-normal[0] * shift, -normal[1] * shift]),   // Line B (start)
 				                    Engine.Vec2.Add(end,   [-normal[0] * shift, -normal[1] * shift]) ]; // Line B (end)
-				Engine.Debug.RegisterDrawCommand(Engine.Debug.line_verts, extra_lines, colour);
+				Engine.Debug.DeferDrawLine(extra_lines, colour);
 			}
 		}
 	},
 
-	// DrawRect : function(position, width, height, colour, camera)
-	// {
-	// 	draw_commands.push(
-	// 	{
-	// 		func   : Engine.Debug.debug_draw_func_rect,
-	// 		config :
-	// 		{
-	// 			position : position,
-	// 			width    : width,
-	// 			height   : height,
-	// 			colour   : colour || Engine.Colour.Red,
-	// 			camera   : camera
-	// 		}
-	// 	});
-	// },
+	DrawPolygon : function(vertices, colour, camera)
+	{
+		Engine.Debug.DeferDrawPoly(vertices, colour);
+	},
 
-	// DrawPolygon : function(vertices, colour, camera)
-	// {
-	// 	draw_commands.push(
-	// 	{
-	// 		func   : Engine.Debug.debug_draw_func_poly,
-	// 		config :
-	// 		{
-	// 			vertices : vertices,
-	// 			colour   : colour || Engine.Colour.Red,
-	// 			camera   : camera
-	// 		}
-	// 	});
-	// },
+	DrawRect : function(position, width, height, colour, camera)
+	{
+		// Setup transform (canvas space)
+		var mtx_trans = mat4.create();
+		var x = position[0] + (width  / 2); // DrawRect is centered by default
+		var y = position[1] + (height / 2); // DrawRect is centered by default
+		mat4.translate(mtx_trans, Engine.Math.IdentityMatrix, [x, y, 0]);
+		mat4.scale(mtx_trans, mtx_trans, [width / 2, height / 2, 0.0]);
 
-	// DrawCircle : function(position, radius, colour, camera)
-	// {
-	// 	draw_commands.push(
-	// 	{
-	// 		func   : Engine.Debug.debug_draw_func_circle,
-	// 		config :
-	// 		{
-	// 			position : position,
-	// 			radius   : radius,
-	// 			colour   : colour || Engine.Colour.Red,
-	// 			camera   : camera
-	// 		}
-	// 	});
-	// },
+		// Deferred draw
+		Engine.Debug.prim_queue.push({ type : "rect", mtx : mtx_trans, colour : colour});
+	},
+
+	DrawCircle : function(position, radius, colour, camera)
+	{
+		// Setup transform (canvas space)
+		var mtx_trans = mat4.create();
+		mat4.translate(mtx_trans, Engine.Math.IdentityMatrix, [position[0], position[1], 0]);
+		mat4.scale(mtx_trans, mtx_trans, [radius, radius, 0.0]);
+
+		// Deferred draw
+		Engine.Debug.prim_queue.push({ type : "circle", mtx : mtx_trans, colour : colour});
+	},
 
 	Update : function()
 	{
 		// Note: This is called prior to any user rendering, so can be
 		//       used to clear out draw commands from previous frame
 		Engine.Debug.draw_commands = [];
-		Engine.Debug.line_verts.stream_index = 0;
-		Engine.Debug.poly_verts.stream_index = 0;
+		Engine.Debug.prim_queue = [];
+		Engine.Debug.soup_idx = 0;
 	},
 
 	Render : function()
 	{
-		if(Engine.Debug.draw_commands.length == 0)
+		// Early out?
+		if(Engine.Debug.draw_commands.length == 0 && Engine.Debug.prim_queue.length == 0)
 			return;
 
 		// Note: This is called post user-rendering so we can disable
@@ -135,11 +150,10 @@ Engine.Debug =
 		Engine.Gfx.EnableBlend(true);
 
 		// Update vertex streams
-		Engine.Gfx.UpdateDynamicVertexBuffer(Engine.Debug.line_verts.vbo, Engine.Debug.line_verts);
-		Engine.Gfx.UpdateDynamicVertexBuffer(Engine.Debug.poly_verts.vbo, Engine.Debug.poly_verts);
+		Engine.Gfx.UpdateDynamicVertexBuffer(Engine.Debug.soup_vbo, Engine.Debug.soup_descriptor);
 
 		// Bind shader
-		Engine.Gfx.BindShaderProgram(Engine.Debug.debug_draw_program_basic);
+		Engine.Gfx.BindShaderProgram(Engine.Debug.shader_program);
 
 		// Setup transform (canvas space)
 		var mtx_proj = mat4.create(); mat4.identity(mtx_proj);
@@ -149,106 +163,30 @@ Engine.Debug =
 		Engine.Gfx.SetShaderConstant("u_trans_view",  Engine.Math.IdentityMatrix, Engine.Gfx.SC_MATRIX4);
 		Engine.Gfx.SetShaderConstant("u_trans_proj",  mtx_proj, Engine.Gfx.SC_MATRIX4);
 
-		// Draw commands (using offsets into main vertex stream)
+		// Draw lines and polys (using offsets into vertex soup)
 		for(var i = 0; i < Engine.Debug.draw_commands.length; ++i)
 		{
 			var command = Engine.Debug.draw_commands[i];
 			Engine.Gfx.SetShaderConstant("u_colour", command.colour, Engine.Gfx.SC_COLOUR);
-			Engine.Gfx.BindVertexBuffer(command.buffer.vbo);
-			Engine.Gfx.DrawArray(command.offset, command.count);
+			Engine.Gfx.BindVertexBuffer(Engine.Debug.soup_vbo);
+			Engine.Gfx.DrawArray(command.offset, command.count, command.draw_mode);
+		}
+
+		// Draw deferred prims (rect & circles)
+		for(var i = 0; i < Engine.Debug.prim_queue.length; ++i)
+		{
+			var prim = Engine.Debug.prim_queue[i];
+			Engine.Gfx.SetShaderConstant("u_colour", prim.colour, Engine.Gfx.SC_COLOUR);
+			Engine.Gfx.SetShaderConstant("u_trans_model", prim.mtx, Engine.Gfx.SC_MATRIX4);
+			switch(prim.type)
+			{
+				case "rect":
+					Engine.Gfx.DrawQuad(); break;
+				case "circle":
+					Engine.Gfx.DrawModel(Engine.Debug.circle_model); break;
+			}
 		}
 	},
-
-	// ****************************************
-	// Draw functions
-	// ****************************************
-	// debug_draw_func_circle : function(config)
-	// {
-	// 	// Bind shader
-	// 	Engine.Gfx.BindShaderProgram(Engine.Debug.debug_draw_program_basic);
-
-	// 	// Setup transform (canvas space)
-	// 	var mtx_trans = mat4.create();
-	// 	mat4.translate(mtx_trans, Engine.Math.IdentityMatrix, [config.position[0], config.position[1], 0]);
-	// 	mat4.scale(mtx_trans, mtx_trans, [config.radius, config.radius, 0.0]);
-	// 	var mtx_proj = mat4.create(); mat4.identity(mtx_proj);
-	// 	var canvas_size = Engine.Canvas.GetSize();
-	// 	mat4.ortho(mtx_proj, 0, canvas_size[0], 0, canvas_size[1], -1, 1);
-	// 	Engine.Gfx.SetShaderConstant("u_trans_model", mtx_trans, Engine.Gfx.SC_MATRIX4);
-	// 	Engine.Gfx.SetShaderConstant("u_trans_view",  Engine.Math.IdentityMatrix, Engine.Gfx.SC_MATRIX4);
-	// 	Engine.Gfx.SetShaderConstant("u_trans_proj",  mtx_proj, Engine.Gfx.SC_MATRIX4);
-
-	// 	// Set colour
-	// 	Engine.Gfx.SetShaderConstant("u_colour", config.colour, Engine.Gfx.SC_COLOUR);
-
-	// 	// Draw circle
-	// 	Engine.Gfx.DrawModel(Engine.Debug.debug_draw_circle_model);
-	// },
-
-	// debug_draw_func_rect : function(config)
-	// {
-		
-
-	// 	// Setup transform (canvas space)
-	// 	var mtx_trans = mat4.create();
-	// 	var x = config.position[0] + (config.width / 2);  // DrawRect is centered by default
-	// 	var y = config.position[1] + (config.height / 2); // DrawRect is centered by default
-	// 	mat4.translate(mtx_trans, Engine.Math.IdentityMatrix, [x, y, 0]);
-	// 	mat4.scale(mtx_trans, mtx_trans, [config.width / 2, config.height / 2, 0.0]);
-	// 	var mtx_proj = mat4.create(); mat4.identity(mtx_proj);
-	// 	var canvas_size = Engine.Canvas.GetSize();
-	// 	mat4.ortho(mtx_proj, 0, canvas_size[0], 0, canvas_size[1], -1, 1);
-	// 	Engine.Gfx.SetShaderConstant("u_trans_model", mtx_trans, Engine.Gfx.SC_MATRIX4);
-	// 	Engine.Gfx.SetShaderConstant("u_trans_view",  Engine.Math.IdentityMatrix, Engine.Gfx.SC_MATRIX4);
-	// 	Engine.Gfx.SetShaderConstant("u_trans_proj",  mtx_proj, Engine.Gfx.SC_MATRIX4);
-
-	// 	// Set colour
-	// 	Engine.Gfx.SetShaderConstant("u_colour", config.colour, Engine.Gfx.SC_COLOUR);
-
-	// 	// Draw rectangle
-	// 	Engine.Gfx.DrawQuad();
-	// },
-
-	// debug_draw_func_poly : function(config)
-	// {
-	// 	var poly_verts =
-	// 	{
-	// 		attribute_name : "a_pos",
-	// 		item_size      : 3,
-	// 		draw_mode      : "triangle_fan",
-	// 		stream         : [],
-	// 	};
-
-	// 	// Setup vertex stream
-	// 	for(var i = 0; i < config.vertices.length; ++i)
-	// 	{
-	// 		var vert = config.vertices[i];
-	// 		poly_verts.stream.push(vert[0]);
-	// 		poly_verts.stream.push(vert[1]);
-	// 		poly_verts.stream.push(0.0);
-	// 	}
-
-	// 	// Bind shader
-	// 	Engine.Gfx.BindShaderProgram(Engine.Debug.debug_draw_program_basic);
-
-	// 	// Create & bind poly vertex stream
-	// 	var poly_vbo = Engine.Gfx.CreateVertexBuffer(poly_verts);
-	// 	Engine.Gfx.BindVertexBuffer(poly_vbo);
-
-	// 	// Setup transform (canvas space)
-	// 	var mtx_proj = mat4.create(); mat4.identity(mtx_proj);
-	// 	var canvas_size = Engine.Canvas.GetSize();
-	// 	mat4.ortho(mtx_proj, 0, canvas_size[0], 0, canvas_size[1], -1, 1);
-	// 	Engine.Gfx.SetShaderConstant("u_trans_model", Engine.Math.IdentityMatrix, Engine.Gfx.SC_MATRIX4);
-	// 	Engine.Gfx.SetShaderConstant("u_trans_view",  Engine.Math.IdentityMatrix, Engine.Gfx.SC_MATRIX4);
-	// 	Engine.Gfx.SetShaderConstant("u_trans_proj",  mtx_proj, Engine.Gfx.SC_MATRIX4);
-
-	// 	// Set colour
-	// 	Engine.Gfx.SetShaderConstant("u_colour", config.colour, Engine.Gfx.SC_COLOUR);
-
-	// 	// Draw line
-	// 	Engine.Gfx.DrawArray();
-	// },
 
 	Break : function()
 	{
