8ab531ac67f91d20f53bc97d6aca265dbcd48e2e
Thomas Sampson <drummertom999@gmail.com>
2015-04-27 21:23:05 +0100

    > Added Engine.Math.AABB2D.Intersects() to test intersection with another AABB2D instance

--- modules/enginejs-math.js
+++ modules/enginejs-math.js
@@ -31,12 +31,22 @@ Engine.Math =
 	{
 		this.min = min;
 		this.max = max;
+
 		this.ContainsPoint = function(point)
 		{
 			if(point[0] < this.min[0] || point[0] > this.max[0])
 				return false;
 			if(point[1] < this.min[1] || point[1] > this.max[1])
 				return false;
+			return true;
+		};
+
+		this.Intersects = function(other_aabb)
+		{
+			if(other_aabb.min[0] > this.max[0] || other_aabb.max[0] < this.min[0])
+				return false; // horizontal separation
+			if(other_aabb.min[1] > this.max[1] || other_aabb.max[1] < this.min[1])
+				return false; // horizontal separation
 			return true;
 		};
 	}
