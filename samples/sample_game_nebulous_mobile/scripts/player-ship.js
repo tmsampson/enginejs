22fadf564ea6719ce4b948189c988f9e76e372b0
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2015-07-19 21:12:46 +0100

    * Added mobile version of nebulous (WIP) * Engine.Spatial.Quadtree now pre-allocates for + perf

---
+++ samples/sample_game_nebulous_mobile/scripts/player-ship.js
@@ -0,0 +1,190 @@
+//# sourceURL=scripts/player-ship.js
+
+Game.PlayerShip = function()
+{
+	// Params
+	this.fire_speed = 500;
+	this.move_speed = 0.2; // 1 = no delay
+	this.bullets = 100;
+	this.shield = new Engine.Game2D.Entity(Game.Resources["spr_ship_shield"]);
+	this.shield_time = 0;
+
+	// Setup and inherit entity (so PlayerShip "is a" Entity)
+	$.extend(this, new Engine.Game2D.Entity(Game.Resources["spr_ship"], "ship"));
+
+	this.Init = function()
+	{
+		// Position
+		var canvas_size = Engine.Canvas.GetSize();
+		this.MoveTo([canvas_size[0] / 2, canvas_size[1] / 6]);
+		this.target_x = this.GetX();
+		this.SetDepth(1); this.SetSize(canvas_size[0]/5);
+
+		//if(Engine.Touch.IsEnabled())
+			//this.move_speed = 0.08;
+
+		// Reset
+		this.state      = "idle";
+		this.health     = 100;
+	};
+
+	this.Update = function(info)
+	{
+		var canvas_size = Engine.Canvas.GetSize();
+		this.SetSize(canvas_size[0]/5);
+		this.shield.SetSize(canvas_size[0]/5);
+
+		// Move
+		var gamepad = Engine.Gamepad.Pads[0];
+		if(Engine.Touch.IsPressed())
+			this.target_x = Engine.Touch.GetX();
+
+		var move_speed = Game.Config.SHIP_MOVE_SPEED * Engine.Canvas.GetWidth();
+		if(Engine.Keyboard.IsPressed("left") || (gamepad && gamepad.IsPressed("left")))
+		{
+			this.target_x = this.GetX() - move_speed;
+		}
+		else if(Engine.Keyboard.IsPressed("right") || (gamepad && gamepad.IsPressed("right")))
+		{
+			this.target_x = this.GetX() + move_speed;
+		}
+
+		if(gamepad)
+		{
+			var stick = gamepad.GetLeftStick()[0];
+			if(Math.abs(stick) > 0.1)
+			{
+				this.target_x = this.GetX() + (stick * move_speed);
+			}
+		}
+
+		// Fire?
+		if(this.IsTapped() || Engine.Keyboard.IsPressed("space", true) || (gamepad && gamepad.IsPressed("a", true)))
+		{
+			this.Fire();
+		}
+
+		var half_width = this.GetSize() / 2;
+		var x_delta = this.target_x - this.GetX();
+		var new_x = this.GetX() + (x_delta * this.move_speed);
+		new_x = Engine.Math.Clamp(new_x, half_width, Engine.Canvas.GetWidth() - half_width);
+		var move_x = new_x - this.GetX();
+		this.MoveTo([new_x, canvas_size[1] / 10]);
+
+		// Set sprite
+		if(move_x < -Game.Config.SHIP_TILT_BACK_SPEED && this.state != "bank_left")
+		{
+			this.sprite.SetSequence("bank_left");
+			this.state = "bank_left";
+		}
+		else if(move_x > Game.Config.SHIP_TILT_BACK_SPEED && this.state != "bank_right")
+		{
+			this.sprite.SetSequence("bank_right");
+			this.state = "bank_right";
+		}
+		else if(Math.abs(move_x) < Game.Config.SHIP_TILT_BACK_SPEED && this.state != "idle")
+		{
+			this.sprite.SetSequence("idle");
+			this.state = "idle";
+		}
+
+		// Remove off-screen bullets
+		for(bullet of Game.GetStarfield().FindByTag("bullet"))
+		{
+			if(bullet.IsOutsideView())
+			{
+				Game.GetStarfield().Remove(bullet);
+			}
+		}
+
+		// Handle ship<-->asteroid collisions
+		var asteroids_touching_ship = Game.GetStarfield().HitTest(this, "asteroid");
+		for(var i = 0; i < asteroids_touching_ship.length; ++i)
+		{
+			var asteroid = asteroids_touching_ship[i];
+			if(!asteroid.is_exploding)
+			{
+				Game.GetStarfield().DestroyAsteroid(asteroids_touching_ship[i]);
+				this.health -= asteroid.damage;
+			}
+		}
+
+		// Handle ammo pickup
+		var collected_ammo = Game.GetStarfield().HitTest(this, "ammo");
+		for(var i = 0; i < collected_ammo.length; ++i)
+		{
+			var ammo = collected_ammo[i];
+			Game.GetStarfield().Remove(ammo);
+			this.bullets = 100;
+			Engine.Audio.PlaySFX(Game.Resources["sfx_ammo_pickup"]);
+		}
+
+		// Handle shield pickup
+		var collected_shield = Game.GetStarfield().HitTest(this, "shield");
+		for(var i = 0; i < collected_shield.length; ++i)
+		{
+			var shield = collected_shield[i];
+			Game.GetStarfield().Remove(shield);
+			this.shield_time = 240;
+			this.shield.SetAlpha(1);
+			Engine.Audio.PlaySFX(Game.Resources["sfx_shield_pickup"]);
+		}
+
+		// Draw shield?
+		if(this.shield_time > 0)
+		{
+			this.shield.SetVisible(true);
+			this.shield.MoveTo(this.GetPosition());
+			this.shield_time--;
+
+			// Low shield?
+			if(this.shield_time < 100)
+			{
+				this.shield.SetAlpha(this.shield_time / 100);
+			}
+
+			// Handle shield<-->asteroid collisions
+			var asteroids_touching_shield = Game.GetStarfield().HitTest(this.shield, "asteroid");
+			for(var i = 0; i < asteroids_touching_shield.length; ++i)
+			{
+				var asteroid = asteroids_touching_shield[i];
+				if(!asteroid.is_exploding && !asteroid.bounced)
+				{
+					var y_reverse = Math.abs(asteroid.GetVelocity()[1]);
+					asteroid.SetVelocity([0, y_reverse]);
+					asteroid.bounced = true;
+					Engine.Audio.PlaySFX(Game.Resources["sfx_bounce"]);
+					Game.GetStarfield().AddScore(100);
+				}
+			}
+		}
+		else
+		{
+			this.shield.SetVisible(false);
+		}
+	};
+
+	this.Fire = function()
+	{
+		if(this.bullets > 0)
+		{
+			var bullet = new Engine.Game2D.Entity(Game.Resources["spr_bullet"], "bullet");
+			bullet.MoveTo(this.GetPosition());
+			bullet.SetVelocity([0, this.fire_speed]);
+			bullet.SetSize(this.GetSize());
+			Game.GetStarfield().Add(bullet);
+			Engine.Audio.PlaySFX(Engine.Math.RandomInteger(0, 1)? Game.Resources["sfx_lazer_1"] : Game.Resources["sfx_lazer_2"]);
+			this.bullets -= 1;
+		}
+		else
+		{
+			// Out of ammo
+			Engine.Audio.PlaySFX(Game.Resources["sfx_no_ammo"]);
+		}
+	};
+
+	this.GetHealth = function()
+	{
+		return this.health;
+	};
+}
---
+++ samples/sample_game_nebulous_mobile/scripts/starfield.js
@@ -0,0 +1,274 @@
+//# sourceURL=scripts/starfield.js
+
+Game.Starfield = function()
+{
+	// Setup and inherit scene (so Starfield "is a" Scene)
+	$.extend(this, new Engine.Game2D.Scene(Game.Resources["bg_space"]));
+	this.layer_scroll_0 = this.background.layers[0].scroll[1];
+	this.layer_scroll_1 = this.background.layers[0].scroll[1];
+
+	// Setup ship
+	this.player_ship = new Game.PlayerShip();
+
+	// Setup score
+	// this.score_text = new Engine.Text2D.TextBox("0",
+	// {
+	// 	prefix : "Score: ",
+	// 	dock   : ["top", "left"],
+	// 	size   : 25,
+	// 	colour : "#48CAFF",
+	// 	css    : Game.Config.FONT_CSS
+	// }); this.score_text.Hide();
+
+	// Setup health
+	// this.health_text = new Engine.Text2D.TextBox(Game.Config.MAX_AMMO,
+	// {
+	// 	prefix : "Health: ",
+	// 	dock   : ["top", "right"],
+	// 	size   : 25,
+	// 	colour : "#FFFFFF",
+	// 	css    : Game.Config.FONT_CSS
+	// }); this.health_text.Hide();
+
+	// Setup ammo icons
+	//this.ammo_icons = [];
+	for(var i = 0; i < Game.Config.MAX_AMMO; ++i)
+	{
+		//this.ammo_icons[i] = new Engine.Game2D.Entity(Game.Resources["tex_ammo_icon"]);
+		//this.ammo_icons[i].SetDepth(-1);
+		//this.Add(this.ammo_icons[i]);
+	}
+
+	this.Enter = function()
+	{
+		// Make sure text is visible
+		this.player_ship.Init();
+		this.SetScore(0);
+		//this.score_text.Show();
+		//this.health_text.Show();
+
+		var canvas_size = Engine.Canvas.GetSize();
+
+		// Enable debug rendering?
+		this.EnableDebugRender(Game.Config.ENABLE_DEBUG_RENDER);
+
+		// Add player ship & ammo icons
+		this.Add(this.player_ship);
+		this.Add(this.player_ship.shield);
+		this.player_ship.bullets = Game.Config.MAX_AMMO;
+		for(var i = 0; i < Game.Config.MAX_AMMO; ++i)
+		{
+			//this.Add(this.ammo_icons[i]);
+		}
+
+		// Setup earth (scenery)
+		var earth = new Engine.Game2D.Entity(Game.Resources["spr_earth"], "scenery");
+		earth.SetVelocity([0, -600]); earth.lifespan = [3, 6];
+		earth.SetDepth(2); earth.parallax = 10;
+		earth.SetSize(Engine.Canvas.GetWidth() / 3);
+		this.Add(earth);
+
+		// Setup asteroids
+		this.asteroid_wait = 0;
+	};
+
+	this.Exit = function()
+	{
+		this.Clear();
+		this.scenery = [];
+		//this.score_text.Hide();
+		//this.health_text.Hide();
+	};
+
+	this.UpdateScenery = function(ship_shift, info)
+	{
+		for(item of this.FindByTag("scenery"))
+		{
+			// Update parallax
+			item.SetX(item.random_x - (ship_shift * item.parallax));
+
+			// Respawn?
+			if(item.wait == undefined || item.wait <= 0)
+			{
+				var half_size = item.GetSize() / 2;
+				item.random_x = Engine.Math.RandomInteger(-half_size, Engine.Canvas.GetWidth() + half_size);
+				item.wait = Engine.Math.Random(item.lifespan[0], item.lifespan[1]);
+				item.MoveTo([item.random_x, Engine.Canvas.GetHeight() + half_size]);
+			}
+
+			// Tick
+			item.wait -= info.delta_s;
+		}
+	};
+
+	this.SpawnAsteroid = function()
+	{
+		// Create a new asteroid & reset spawn timer
+		var asteroid = new Engine.Game2D.Entity(Game.Resources["spr_asteroid"], "asteroid");
+		this.asteroid_wait = Engine.Math.RandomInteger(30, 120);
+
+		// Move asteroid into place (off-screen)
+		asteroid.MoveTo([Engine.Math.RandomInteger(0, Engine.Canvas.GetWidth()), Engine.Canvas.GetHeight() + 100]);
+
+		// Randomly size the asteroid
+		var min_size = Game.Config.ASTEROID_SIZE_MIN * Engine.Canvas.GetWidth();
+		var max_size = Game.Config.ASTEROID_SIZE_MAX * Engine.Canvas.GetWidth();
+		var size = Engine.Math.RandomInteger(min_size, max_size);
+		asteroid.SetSize(size);
+
+		// Randomly swap the sprite sequence used to draw the asteroid
+		var swap = Engine.Math.RandomInteger(0, 3);
+		if(swap == 0)
+		{
+			asteroid.sprite.SetSequence("asteroid_2");
+		}
+		else if(swap == 1)
+		{
+			asteroid.sprite.SetSequence("asteroid_3");
+		}
+
+		// Start the asteroid moving at a speed proportional to it's size (small asteroids fly quicker)
+		var speed = Game.Config.ASTEROID_SPEED_MIN + (max_size / size) * Game.Config.ASTEROID_SPEED_SCALE;
+		asteroid.SetVelocity([0, -speed]);
+
+		// Try and match the anmation speed with the velocity
+		asteroid.sprite.SetSpeed(speed / 50);
+
+		// Setup a "damage" amount for this asteroid based on it's random size
+		asteroid.damage = 5; // Min damage = 5
+		asteroid.damage += 5 * Math.floor(((size - min_size) / (max_size - min_size)) * 3); // Max damage = 20
+		asteroid.health = asteroid.damage / 5; // Max hits rqd = 3;
+
+		// Add the asteroid to the scene
+		this.Add(asteroid);
+	};
+
+	this.DestroyAsteroid = function(asteroid, increase_score)
+	{
+		if(!asteroid.is_exploding)
+		{
+			asteroid.is_exploding = true;
+			asteroid.sprite.SetSequence("explosion");
+			asteroid.SetAlpha(0.4);
+			Engine.Audio.PlaySFX(Game.Resources["sfx_explosion"]);
+			if(increase_score)
+			{
+				this.AddScore(asteroid.damage * (asteroid.bounced? 10 : 1));
+			}
+		}
+	};
+
+	this.Update = function(info)
+	{
+		this.player_ship.Update(info);
+		var ship_shift = ((this.player_ship.GetX() / Engine.Canvas.GetWidth()) * 2) - 1; // -1 --> 1
+
+		// Update parallax effect for background / scenery
+		this.background.layers[0].scroll[1] = this.layer_scroll_0 * (Engine.Canvas.GetHeight() / 1000);
+		this.background.layers[1].scroll[1] = this.layer_scroll_1 * (Engine.Canvas.GetHeight() / 1000);
+		for(layer of this.background.layers)
+		{
+			var layer_multiplier = 1 / (layer.depth + 1);
+			layer.offset[0] = -(ship_shift * layer_multiplier * Game.Config.PARALAX_AMOUNT);
+		}
+
+		// Update scenery
+		this.UpdateScenery(ship_shift, info);
+
+		// Spawn more asteroids?
+		if(--this.asteroid_wait < 0)
+			this.SpawnAsteroid();
+
+		// Update asteroids
+		for(asteroid of this.FindByTag("asteroid"))
+		{
+			// Skip exploding asteroids (removing if explosion is complete)
+			if(asteroid.is_exploding)
+			{
+				if(asteroid.sprite.SequenceHasFinished())
+					this.Remove(asteroid);
+				continue;
+			}
+
+			// Remove off-screen asteroids
+			if(asteroid.GetY() < -(asteroid.GetSize() / 2))
+			{
+				this.Remove(asteroid);
+				continue;
+			}
+
+			// Handle asteroid<-->bullet collisions (for on-screen asteroids only)
+			if(asteroid.IsWithinView())
+			{
+				var bullets = this.HitTest(asteroid, "bullet");
+				if(bullets.length > 0)
+				{
+					asteroid.health -= 1;
+					this.Remove(bullets);
+					if(asteroid.health == 0)
+					{
+						this.DestroyAsteroid(asteroid, true);
+					}
+					else
+					{
+						Engine.Audio.PlaySFX(Game.Resources["sfx_asteroid_hit"]);
+					}
+				}
+			}
+		}
+
+		// Drop ammo?
+		if(Engine.Math.RandomInteger(0, 500) == 0)
+		{
+			var ammo = new Engine.Game2D.Entity(Game.Resources["spr_pickup"], "ammo");
+			ammo.MoveTo([Engine.Math.RandomInteger(0, Engine.Canvas.GetWidth()), Engine.Canvas.GetHeight() + 100]);
+			ammo.SetVelocity([0, -600]);
+			ammo.sprite.SetSequence("ammo_pickup");
+			this.Add(ammo);
+		}
+
+		// Drop shield?
+		if(Engine.Math.RandomInteger(0, 320) == 0)
+		{
+			var shield = new Engine.Game2D.Entity(Game.Resources["spr_pickup"], "shield");
+			shield.MoveTo([Engine.Math.RandomInteger(0, Engine.Canvas.GetWidth()), Engine.Canvas.GetHeight() + 100]);
+			shield.SetVelocity([0, -600]);
+			shield.sprite.SetSequence("shield_pickup");
+			this.Add(shield);
+		}
+
+		// Draw ship health
+		var health = this.player_ship.GetHealth();
+		//this.health_text.Set(health);
+		if(health <= 0)
+		{
+			Game.SetState("Score");
+		}
+
+		// Draw ammo icons
+		var spacing = 6;
+		var left = Engine.Canvas.GetCentre()[0] - (spacing * this.player_ship.bullets) / 2;
+		for(var i = 0; i < Game.Config.MAX_AMMO; ++i)
+		{
+			//this.ammo_icons[i].SetVisible(i < this.player_ship.bullets);
+			//this.ammo_icons[i].MoveTo([left + i * spacing, 16]);
+		}
+	};
+
+	this.SetScore = function(amount)
+	{
+		this.score = amount;
+		//this.score_text.Set(this.score);
+	};
+
+	this.AddScore = function(amount)
+	{
+		this.score += amount;
+		//this.score_text.Set(this.score);
+	};
+
+	this.GetScore = function()
+	{
+		return this.score;
+	};
+};
