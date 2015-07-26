49ff16f67599a59e27003b61423090250bbe5e57
Thomas Sampson <drummertom999@gmail.com>
2015-07-26 11:11:09 +0100

    > Tidied up Engine.Spatial.QuadTree

--- modules/enginejs-spatial.js
+++ modules/enginejs-spatial.js
@@ -3,69 +3,101 @@
 // *******************************************
 Engine.Spatial =
 {
-	QuadTree : function(min, max, max_items_per_node)
+	// ==========================================================================================================
+	// ==========================================================================================================
+	//  Type : QuadTree
+	// ==========================================================================================================
+	// ==========================================================================================================
+	QuadTree : function(min, max, max_items_per_node, max_node_count)
 	{
-		this.min = min || [-10000000, -10000000];
-		this.max = max || [ 10000000,  10000000];
-		this.max_items_per_node = max_items_per_node || 4;
-		this.subdivide_count = 0;
-		this.nodes = [];
-		this.FREE_NODE = 0;
+		// ------------------------------------------------------------------------------------------------------
+		// Global constants
+		this.DEFAULT_WORLD_MIN = [-10000000, -10000000];
+		this.DEFAULT_WORLD_MAX = [ 10000000,  10000000];
+		this.DEFAULT_MAX_NODE_COUNT = 50000;
+		this.DEFAULT_MAX_ITEMS_PER_NODE = 4;
 
-		this.MAKE_NODE = function(parent, min, max)
+		// ------------------------------------------------------------------------------------------------------
+		// Set tree properties
+		this.world_min = min || this.DEFAULT_WORLD_MIN;
+		this.world_max = max || this.DEFAULT_WORLD_MAX;
+		this.max_node_count = max_node_count || this.DEFAULT_MAX_NODE_COUNT;
+		this.max_items_per_node = max_items_per_node || this.DEFAULT_MAX_ITEMS_PER_NODE;
+
+		// ------------------------------------------------------------------------------------------------------
+		// Pre-allocate tree nodes
+		this.nodes = []; this.free_node_idx = 0;
+		for(var i = 0; i < this.max_node_count; ++i)
 		{
-			var available = this.nodes[this.FREE_NODE++];
-			available.parent = parent;
-			available.min = min;
-			available.max = max;
-			return available;
+			this.nodes[i] = new Engine.Spatial.QuadTreeNode(null, this.world_min, this.world_max, this);
 		}
 
-		this.Init = function()
+		// ------------------------------------------------------------------------------------------------------
+		// Internal node management functions
+		this.CreateNode = function(parent, min, max)
 		{
-			// Setup default state
-			this.root = this.MAKE_NODE(null, this.min, this.max);
-		};
-
-		// Pre-allocate
-		for(var i = 0; i < 10000; ++i)
-		{
-			this.nodes[i] = new Engine.Spatial.QuadTreeNode(null, 0, 0, this.max_items_per_node, this);
-		}
-		this.Init();
-
-		this.Clear = function()
-		{
-			this.FREE_NODE = 0;
-			this.subdivide_count = 0;
-			for(var i = 0; i < 10000; ++i)
+			// Do we have a node available?
+			if(this.free_node_idx >= this.max_node_count)
 			{
-				this.nodes[i].items = [];
-				this.nodes[i].children = [];
+				Engine.LogError("Quadtree ran out of nodes, consider increasing the maximum node count");
+				return null;
 			}
-			this.Init();
+
+			// Grab a free node
+			var available_node = this.nodes[this.free_node_idx++];
+
+			// Set node attributes
+			available_node.parent = parent;
+			available_node.min = min;
+			available_node.max = max;
+			return available_node;
 		};
 
+		// ------------------------------------------------------------------------------------------------------
+		// Public node management functions
 		this.Add = function(item, min, max)
 		{
 			this.root.Add(item, min, max);
 		};
 
-		this.Retrieve = function(min, max)
+		this.Search = function(min, max)
 		{
-			return this.root.Retrieve(min, max);
+			return this.root.Search(min, max);
 		};
 
-		this.GetSubdivideCount = function()
+		this.Clear = function()
 		{
-			return this.subdivide_count;
+			// Mark all nodes as re-usable
+			this.free_node_idx = 0;
+			for(var i = 0; i < this.max_node_count; ++i)
+			{
+				this.nodes[i].items = [];
+				this.nodes[i].child_nodes = [];
+			}
+
+			// Reset root node
+			this.root = this.CreateNode(null, this.world_min, this.world_max);
+
+			// Reset debug stats
+			this.dbg_subdivide_count = 0;
+		};
+		this.Clear(); // Clear on initialisation
+
+		// ------------------------------------------------------------------------------------------------------
+		// Debug tracking
+		this.dbg_subdivide_count = 0;
+		this.GetDebugInfo = function()
+		{
+			return { subdivide_count : this.dbg_subdivide_count };
 		};
 
+		// ------------------------------------------------------------------------------------------------------
+		// Debug rendering
 		this.DebugRender = function()
 		{
 			var debug_render_node = function(node)
 			{
-				if(node.children.length)
+				if(node.child_nodes.length)
 				{
 					var node_width  = node.max[0] - node.min[0];
 					var node_height = node.max[1] - node.min[1];
@@ -75,67 +107,110 @@ Engine.Spatial =
 					                      [node.min[0] + (node_width/2), node.max[1]], Engine.Colour.Blue, 1);
 				}
 
-				for(var i = 0; i < node.children.length; ++i)
+				for(var i = 0; i < node.child_nodes.length; ++i)
 				{
-					debug_render_node(node.children[i]);
+					debug_render_node(node.child_nodes[i]);
 				}
 			};
 			debug_render_node(this.root);
 		};
 	},
 
-	QuadTreeNode : function(parent, min, max, max_items, tree)
+	// ==========================================================================================================
+	// ==========================================================================================================
+	//  Type : QuadTreeNode
+	// ==========================================================================================================
+	// ==========================================================================================================
+	QuadTreeNode : function(parent, min, max, tree)
 	{
-		this.parent = parent;
+		// ------------------------------------------------------------------------------------------------------
+		// Set node properties
+		this.tree = tree;     // Owner tree
+		this.parent = parent; // Parent-node
 		this.min = min; this.max = max;
-		this.max_items = max_items;
-		this.items = [];
-		this.children = [];
-		this.tree = tree;
 
-		this.HasChildren = function()
-		{
-			return (this.children.length != 0);
-		};
+		// ------------------------------------------------------------------------------------------------------
+		// Node contents
+		this.items       = [];
 
-		this.IsFull = function()
-		{
-			return (this.items.length == this.max_items);
-		};
+		// ------------------------------------------------------------------------------------------------------
+		// Node heirarchy
+		this.child_nodes = [];
 
+		// ------------------------------------------------------------------------------------------------------
+		// Public add / Search functions
 		this.Add = function(item)
 		{
 			// Do I need to split?
-			if(!this.HasChildren() && this.IsFull())
+			if(!this.HasChildNodes() && this.IsFull())
 			{
 				this.Split();
 			}
 
 			// Can we add to a child node?
-			if(this.HasChildren() && this.TryAddToChild(item))
+			if(this.HasChildNodes() && this.TryAddToChild(item))
 			{
 				return;
 			}
 
-			// No children or item is not contained fully in child, store in this node
+			// Couldn't fit the item completely into any child nodes so store in this node instead
 			this.Insert(item);
 		};
 
+		this.Search = function(min, max)
+		{
+			// Add all items from this node
+			var results = [];
+			for(var i = 0; i < this.items.length; ++i)
+			{
+				results.push(this.items[i].data);
+			}
+
+			// Gather items from child nodes
+			if(this.HasChildNodes())
+			{
+				// Look for a suitable child node
+				for(var i = 0; i < this.child_nodes.length; ++i)
+				{
+					var child_node = this.child_nodes[i];
+					if(child_node.FullyContainsRegion({min : min, max : max}))
+					{
+						results = results.concat(child_node.Search(min, max));
+						return results;
+					}
+				}
+
+				// If no child node fully contained the search region, we need to
+				// drill down into each child node
+				for(var i = 0; i < this.child_nodes.length; ++i)
+				{
+					results = results.concat(this.child_nodes[i].Search(min, max));
+				}
+			}
+
+			return results;
+		};
+
+		// ------------------------------------------------------------------------------------------------------
+		// Internal item / child management functions
 		this.Insert = function(item)
 		{
+			// All items held in this node have a back-pointer to the qtree node
 			item.qtree_node = this;
+
+			// Add item
 			this.items.push(item);
 		};
 
 		this.TryAddToChild = function(item)
 		{
 			// Look for a suitable child node
-			for(var i = 0; i < this.children.length; ++i)
+			for(var i = 0; i < this.child_nodes.length; ++i)
 			{
-				var child = this.children[i];
-				if(child.FullyContainsItem(item))
+				var child_node = this.child_nodes[i];
+				if(child_node.FullyContainsRegion(item))
 				{
-					child.Add(item);
+					child_node.Add(item);
 					return true;
 				}
 			}
@@ -145,17 +220,19 @@ Engine.Spatial =
 
 		this.Split = function()
 		{
-			++this.subdivide_count;
+			++this.dbg_subdivide_count;
 
 			var min = this.min;
 			var max = this.max;
-			var child_size = [(max[0] - min[0]) / 2, (max[1] - min[1]) / 2];
+
+			var child_width  = (max[0] - min[0]) / 2;
+			var child_height = (max[1] - min[1]) / 2;
 
 			// Setup child nodes
-			this.children.push(tree.MAKE_NODE(this, min, [min[0] + child_size[0], min[1] + child_size[1]])); // BL
-			this.children.push(tree.MAKE_NODE(this, [min[0] + child_size[0], min[1]], [max[0], min[1] + child_size[1]])); // BR
-			this.children.push(tree.MAKE_NODE(this, [min[0], min[1] + child_size[1]], [min[0] + child_size[0], max[1]])); // TL
-			this.children.push(tree.MAKE_NODE(this, [min[0] + child_size[0], min[1] + child_size[1]], max)); // TR
+			this.child_nodes.push(tree.CreateNode(this, min, [min[0] + child_width, min[1] + child_height])); // BL
+			this.child_nodes.push(tree.CreateNode(this, [min[0] + child_width, min[1]], [max[0], min[1] + child_height])); // BR
+			this.child_nodes.push(tree.CreateNode(this, [min[0], min[1] + child_height], [min[0] + child_width, max[1]])); // TL
+			this.child_nodes.push(tree.CreateNode(this, [min[0] + child_width, min[1] + child_height], max)); // TR
 
 			// Distribute items
 			var items = Engine.Array.Copy(this.items);
@@ -169,44 +246,25 @@ Engine.Spatial =
 			}
 		};
 
-		this.FullyContainsItem = function(item)
+		// ------------------------------------------------------------------------------------------------------
+		// Helper functions
+		this.HasChildNodes = function()
+		{
+			return (this.child_nodes.length != 0);
+		};
+
+		this.IsFull = function()
+		{
+			return (this.items.length == this.tree.max_items_per_node);
+		};
+
+		this.FullyContainsRegion = function(item)
 		{
 			if(item.min[0] < this.min[0] || item.min[1] < this.min[1])
 				return false;
 			if(item.max[0] > this.max[0] || item.max[1] > this.max[1])
 				return false;
 			return true;
-		};
-
-		this.Retrieve = function(min, max)
-		{
-			// Add items from this node
-			var results = [];
-			for(var i = 0; i < this.items.length; ++i)
-			{
-				results.push(this.items[i].data);
-			}
-
-			if(this.HasChildren())
-			{
-				// Look for a suitable child node
-				for(var i = 0; i < this.children.length; ++i)
-				{
-					var child = this.children[i];
-					if(child.FullyContainsItem({min : min, max : max}))
-					{
-						results = results.concat(child.Retrieve(min, max));
-						return results;
-					}
-				}
-
-				for(var i = 0; i < this.children.length; ++i)
-				{
-					results = results.concat(this.children[i].Retrieve(min, max));
-				}
-			}
-
-			return results;
 		};
 	}
 };
