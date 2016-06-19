f31202875dd9f694f6de3d0bdc5cfa74e1f16a46
Thomas Sampson <drummertom999@gmail.com>
2016-06-19 14:26:14 +0100

    Legacy material tidy up

--- resources/shaders/3d/material-legacy.fs
+++ resources/shaders/3d/material-legacy.fs
@@ -11,27 +11,39 @@ uniform vec3 u_cam_pos;
 // Global lighting
 uniform vec3 u_sun_ambient;
 uniform vec3 u_sun_diffuse;
-uniform vec3 u_sun_phong;
+uniform vec3 u_sun_specular;
 uniform vec3 u_sun_dir; // normalised
 
 // Material properties
 uniform vec4 u_material_colour;
+uniform vec4 u_material_specular;
 uniform float u_material_shininess;
 uniform sampler2D u_material_tx_albedo;
 
 void main(void)
 {
-	// Ambient term
+	// Albedo (unlit)
 	vec4 material_albedo = texture2D(u_material_tx_albedo, v_uv.xy) * u_material_colour;
-	vec4 ambient = material_albedo * vec4(u_sun_ambient, 1.0);
 
+	// Ambient term
+	vec4 ambient = material_albedo * vec4(u_sun_ambient, 1.0);
+	vec4 final_colour = ambient;
+
+#ifdef USE_DIFFUSE
 	// Diffuse term
 	vec4 diffuse = material_albedo * vec4(u_sun_diffuse, 1.0) * max(0.0, dot(v_world_normal, -u_sun_dir));
+	final_colour += diffuse;
+#endif
 
-	// Phong term
-	vec3 reflected_ray = reflect(u_sun_dir, v_world_normal); // reflect light about surface normal
-	vec3 to_cam = u_cam_pos - v_world_pos.xyz;
-	float cam_dot = max(dot(normalize(reflected_ray), normalize(to_cam)), 0.0);
-	vec4 phong = material_albedo * clamp(vec4(u_sun_phong, 1.0) * pow(cam_dot, 0.3 * u_material_shininess), 0.0, 1.0);
-	gl_FragColor = ambient + diffuse + phong;
+#ifdef USE_SPECULAR
+	// Specular term
+	vec3 reflected_ray = normalize(reflect(u_sun_dir, v_world_normal)); // reflect light about surface normal
+	vec3 to_cam = normalize(u_cam_pos - v_world_pos.xyz);
+	float cam_dot = max(dot(reflected_ray, to_cam), 0.0);
+	vec4 specular = material_albedo * u_material_specular * clamp(vec4(u_sun_specular, 1.0) * pow(cam_dot, u_material_shininess), 0.0, 1.0);
+	final_colour += specular;	
+#endif
+
+	// Composite
+	gl_FragColor = final_colour;
 }
