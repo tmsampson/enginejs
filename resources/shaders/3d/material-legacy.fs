5b9cbc8b9e5d4a6f2df6e04c106a76026833cfd1
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2016-08-28 15:11:20 +0100

    More work on old-school material system

--- resources/shaders/3d/material-legacy.fs
+++ resources/shaders/3d/material-legacy.fs
@@ -19,6 +19,7 @@ uniform vec3 u_sun_dir; // normalised
 uniform vec4 u_material_colour;
 uniform vec4 u_material_specular;
 uniform float u_material_shininess;
+uniform float u_material_normal_strength;
 
 // Material samplers
 uniform sampler2D u_material_tx_albedo;
@@ -29,10 +30,9 @@ uniform sampler2D u_material_tx_normal;
 
 void main(void)
 {
-	// Albedo (unlit)
-	vec4 material_albedo = texture2D(u_material_tx_albedo, v_uv.xy) * u_material_colour;
-
-	// Grab normal?
+	// *************************************************************************************
+	// Normal
+	// *************************************************************************************
 #ifdef USE_NORMAL_MAP
 	vec4 material_normal = texture2D(u_material_tx_normal, v_uv.xy);
 	vec3 v_world_bitangent = cross(v_world_normal, v_world_tangent);
@@ -43,32 +43,45 @@ void main(void)
 	normal_basis[1] = normalize(v_world_bitangent);
 	normal_basis[2] = normalize(v_world_normal);
 
-	vec3 normal = normal_basis * normalize((material_normal.xyz * 2.0) - 1.0) * 0.6;
+	vec3 normal = normal_basis * normalize((material_normal.xyz * 2.0) - 1.0);
 	
 #else
 	// Use vertex normals
 	vec3 normal = v_world_normal;
 #endif
 
-	// Ambient term
-	vec4 ambient = material_albedo * vec4(u_sun_ambient, 1.0);
-	vec4 final_colour = ambient;
+	// Scale normal
+	normal *= u_material_normal_strength;
 
+	// *************************************************************************************
+	// Ambient
+	// *************************************************************************************
+	vec4 material_albedo = texture2D(u_material_tx_albedo, v_uv.xy) * u_material_colour;
+	vec4 ambient = material_albedo * vec4(u_sun_ambient, 1.0);
+
+	// *************************************************************************************
+	// Diffuse
+	// *************************************************************************************
 #ifdef USE_DIFFUSE
-	// Diffuse term
 	vec4 diffuse = material_albedo * vec4(u_sun_diffuse, 1.0) * max(0.0, dot(normal, -u_sun_dir));
-	final_colour += diffuse;
+#else
+	vec4 diffuse = vec4(0.0);
 #endif
 
+	// *************************************************************************************
+	// Speular
+	// *************************************************************************************
+	vec4 specular = vec4(0.0);
 #ifdef USE_SPECULAR
-	// Specular term
-	vec3 reflected_ray = normalize(reflect(u_sun_dir, normal)); // reflect light about surface normal
-	vec3 to_cam = normalize(u_cam_pos - v_world_pos.xyz);
-	float cam_dot = clamp(dot(reflected_ray, to_cam), 0.0, 1.0);
-	vec4 specular = material_albedo * u_material_specular * clamp(vec4(u_sun_specular, 1.0) * pow(cam_dot, u_material_shininess), 0.0, 1.0);
-	final_colour += specular;
+	if(dot(u_sun_dir, normal) < 0.0)
+	{
+		vec3 reflected_ray = normalize(reflect(u_sun_dir, normal)); // reflect light about surface normal
+		vec3 to_cam = normalize(u_cam_pos - v_world_pos.xyz);
+		float cam_dot = max(0.0, dot(reflected_ray, to_cam));
+		specular = material_albedo * u_material_specular * clamp(vec4(u_sun_specular, 1.0) * pow(cam_dot, u_material_shininess * 128.0), 0.0, 1.0);
+	}
 #endif
 
 	// Composite
-	gl_FragColor = final_colour;
+	gl_FragColor = ambient + diffuse + specular;
 }
