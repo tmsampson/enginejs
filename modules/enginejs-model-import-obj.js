ac1569020bf86257ab81bbbfd17c978fea78f205
Thomas Sampson <drummertom999@gmail.com>
2017-06-28 16:33:36 +0100

    Began re-structuring obj model loader to first load materials file (if present)

--- modules/enginejs-model-import-obj.js
+++ modules/enginejs-model-import-obj.js
@@ -4,284 +4,339 @@
 
 Engine.Model.Importers.OBJ =
 {
-	Load : function(descriptor, callback)
+	LoadMaterialsFile : function(materials_file, callback)
 	{
-		// Grab scale factor  from descriptor (optional)
-		var scale = Engine.Util.IsDefined(descriptor.scale)? descriptor.scale : 1.0;
+		var obj_materials = [];
 
-		// Go grab the obj file
-		Engine.Net.FetchResource(descriptor.file, function(obj_data)
+		// Go grab the .obj file
+		Engine.Net.FetchResource(materials_file, function(materials_file_data)
 		{
-			// Trim excess whitespace
-			obj_data = obj_data.replace(/ +(?= )/g, '');
+			// Trim excess whitespace and split into lines
+			materials_file_data = materials_file_data.replace(/ +(?= )/g, '');
+			var lines = materials_file_data.split("\n");
 
 			// Setup parse
-			var obj_prims = [];
-			var material_file = "";
-			var obj_current_prim = null;
-			var obj_current_material = "";
-			var is_parsing_faces = true;
-
-			// Global pos / uv / normal banks
-			var obj_file_vertices = [];
-			var obj_file_uvs = [];
-			var obj_file_normals = [];
+			var current_material = null;
 
 			// Parse
-			var lines = obj_data.split("\n");
 			for(var i = 0; i < lines.length; ++i)
 			{
 				var line = lines[i].trim();
 
-				// Next prim?
-				if(is_parsing_faces && line[0] != "f")
-				{
-					obj_current_prim =
-					{
-						name     : "none",
-						faces    : [],
-					};
-
-					if(obj_current_material != "")
-					{
-						obj_current_prim.material = obj_current_material;
-					}
-
-					obj_prims.push(obj_current_prim);
-				}
-				is_parsing_faces = false;
-
-				// Grab material library file?
-				if(line.substr(0, 6) == "mtllib")
+				// new material found?
+				if(line.substr(0, 6) == "newmtl")
 				{
 					var values = line.split(" ");
-					material_file = values[1];
-				}
-
-				// Grab material for this prim?
-				if(line.substr(0, 6) == "usemtl")
-				{
-					var values = line.split(" ");
+					var material_name = values[1];
 
 					// Apply to this prim *and* all subsequent prims
-					obj_current_material = values[1];
+					current_material = new Engine.Gfx.Material();
+					current_material.SetColour("albedo_colour", Engine.Colour.Random());
+
+					// Store new material in bank
+					obj_materials[material_name] = current_material;
+				}
+			}
+
+			callback(obj_materials);
+		});
+	},
+
+	LoadModelFile : function(descriptor, lines, loaded_obj_materials)
+	{
+		// Grab scale factor from descriptor (optional)
+		var scale = Engine.Util.IsDefined(descriptor.scale)? descriptor.scale : 1.0;
+
+		// Setup parse
+		var obj_prims = [];
+		var material_file = "";
+		var obj_current_prim = null;
+		var obj_current_material = "";
+		var is_parsing_faces = true;
+
+		// Global pos / uv / normal banks
+		var obj_file_vertices = [];
+		var obj_file_uvs = [];
+		var obj_file_normals = [];
+
+		// Parse
+		for(var i = 0; i < lines.length; ++i)
+		{
+			var line = lines[i].trim();
+
+			// Next prim?
+			if(is_parsing_faces && line[0] != "f")
+			{
+				obj_current_prim =
+				{
+					name     : "none",
+					faces    : [],
+				};
+
+				if(obj_current_material != "")
+				{
 					obj_current_prim.material = obj_current_material;
 				}
 
-				// Skip comments
-				if(line[0] == "#") { continue; }
+				obj_prims.push(obj_current_prim);
+			}
+			is_parsing_faces = false;
 
-				// Process vertex
-				if(line[0] == "v" && line[1] == " ")
-				{
-					var values = line.split(" ");
-					var p0 = parseFloat(values[1]) * scale;
-					var p1 = parseFloat(values[2]) * scale;
-					var p2 = parseFloat(values[3]) * scale;
-					obj_file_vertices.push(p0, p1, p2);
-				}
+			// Grab material for this prim?
+			if(line.substr(0, 6) == "usemtl")
+			{
+				var values = line.split(" ");
 
-				// Process parameter space vertices
-				if(line[0] == "v" && line[1] == "p")
-				{
-					// Not currently supported
-				}
-
-				// Process texture co-ordinates
-				if(line[0] == "v" && line[1] == "t")
-				{
-					var values = line.split(" ");
-					var u = parseFloat(values[1]);
-					var v = parseFloat(values[2]);
-					obj_file_uvs.push(u, v);
-				}
-
-				// Process normals
-				if(line[0] == "v" && line[1] == "n")
-				{
-					var values = line.split(" ");
-					var nx = parseFloat(values[1]);
-					var ny = parseFloat(values[2]);
-					var nz = parseFloat(values[3]);
-					obj_file_normals.push(nx, ny, nz);
-				}
-
-				// Process object / group name
-				if(line[0] == "o" || (line[0] == "g" && obj_current_prim.name == "none"))
-				{
-					obj_current_prim.name = line.substring(2).trim();
-				}
-
-				// Process face
-				if(line[0] == "f")
-				{
-					is_parsing_faces = true;
-					var values = line.split(" ");
-
-					if(values.length == 4)
-					{
-						// Triangle
-						obj_current_prim.faces.push(values[1], values[2], values[3]);
-					}
-					else if(values.length > 4)
-					{
-						// Quad
-						obj_current_prim.faces.push(values[1], values[2], values[3]);
-						obj_current_prim.faces.push(values[1], values[3], values[4]);
-					}
-				}
+				// Apply to this prim *and* all subsequent prims
+				obj_current_material = values[1];
+				obj_current_prim.material = obj_current_material;
 			}
 
-			// Load
-			if(material_file != "")
-			{
+			// Skip comments
+			if(line[0] == "#") { continue; }
 
+			// Process vertex
+			if(line[0] == "v" && line[1] == " ")
+			{
+				var values = line.split(" ");
+				var p0 = parseFloat(values[1]) * scale;
+				var p1 = parseFloat(values[2]) * scale;
+				var p2 = parseFloat(values[3]) * scale;
+				obj_file_vertices.push(p0, p1, p2);
 			}
 
-			// Trim last prim if empty
-			if(obj_current_prim.faces.length == 0)
+			// Process parameter space vertices
+			if(line[0] == "v" && line[1] == "p")
 			{
-				obj_prims.pop();
+				// Not currently supported
 			}
 
-			// ******************************************************************************************
-			// ******************************************************************************************
-			// ******************************************************************************************
-			// ******************************************************************************************
-			// ******************************************************************************************
-			// ******************************************************************************************
-			// ******************************************************************************************
-
-			// Build enginejs model
-			var model_prims = []
-			var model_file =
+			// Process texture co-ordinates
+			if(line[0] == "v" && line[1] == "t")
 			{
-				name: descriptor.file,
-				materials : [],
-				model_data:
+				var values = line.split(" ");
+				var u = parseFloat(values[1]);
+				var v = parseFloat(values[2]);
+				obj_file_uvs.push(u, v);
+			}
+
+			// Process normals
+			if(line[0] == "v" && line[1] == "n")
+			{
+				var values = line.split(" ");
+				var nx = parseFloat(values[1]);
+				var ny = parseFloat(values[2]);
+				var nz = parseFloat(values[3]);
+				obj_file_normals.push(nx, ny, nz);
+			}
+
+			// Process object / group name
+			if(line[0] == "o" || (line[0] == "g" && obj_current_prim.name == "none"))
+			{
+				obj_current_prim.name = line.substring(2).trim();
+			}
+
+			// Process face
+			if(line[0] == "f")
+			{
+				is_parsing_faces = true;
+				var values = line.split(" ");
+
+				if(values.length == 4)
 				{
-					primitives : model_prims
-				},
+					// Triangle
+					obj_current_prim.faces.push(values[1], values[2], values[3]);
+				}
+				else if(values.length > 4)
+				{
+					// Quad
+					obj_current_prim.faces.push(values[1], values[2], values[3]);
+					obj_current_prim.faces.push(values[1], values[3], values[4]);
+				}
+			}
+		}
+
+		// Load
+		if(material_file != "")
+		{
+
+		}
+
+		// Trim last prim if empty
+		if(obj_current_prim.faces.length == 0)
+		{
+			obj_prims.pop();
+		}
+
+		// ******************************************************************************************
+		// ******************************************************************************************
+		// ******************************************************************************************
+		// ******************************************************************************************
+		// ******************************************************************************************
+		// ******************************************************************************************
+		// ******************************************************************************************
+
+		// Build enginejs model
+		var model_prims = []
+		var model_file =
+		{
+			name: descriptor.file,
+			materials : loaded_obj_materials,
+			model_data:
+			{
+				primitives : model_prims
+			},
+		};
+
+		// Expand / assemble obj primitive streams
+		for(var i = 0; i < obj_prims.length; ++i)
+		{
+			// Grab the obj prim
+			var obj_prim = obj_prims[i];
+
+			// Setup equivalent model prim
+			var model_prim_vertex_buffers = [];
+			var model_prim = 
+			{
+				name           : obj_prim.name,
+				vertex_buffers : model_prim_vertex_buffers
 			};
 
-			// Dummy materials (REMOVE THIS!!)
-			var dummy_materials = [ Engine.Resources["mat_standard_default"], Engine.Resources["mat_standard_sponge"] ];
-			var dummy_material_index = 0;
-
-			// Expand / assemble obj primitive streams
-			for(var i = 0; i < obj_prims.length; ++i)
+			if(Engine.Util.IsDefined(obj_prim.material))
 			{
-				// Grab the obj prim
-				var obj_prim = obj_prims[i];
+				model_prim.material = obj_prim.material;
+			}
+			model_prims.push(model_prim);
 
-				// Setup equivalent model prim
-				var model_prim_vertex_buffers = [];
-				var model_prim = 
+			// Setup streams for model buffers
+			// Note: The faces specified in the obj file are not directly compatible with webgl index buffers as they reference mixed length
+			//       streams. For example the obj might have 10 vertices, 4 uvs, 4 normals and 20 faces. For this reason we
+			//       fully expand all streams and don't use an index buffer. Maybe this could be smarter and figure out whether or not
+			//       it's worth trying to assemble an index buffer for the expanded data!
+			var prim_vertices = [];
+			var prim_uvs = [];
+			var prim_normals = [];
+
+			// For each obj file face...
+			for(var face_index = 0; face_index < obj_prim.faces.length; face_index +=3)
+			{
+				// For each of the 3 vertices on this this face...
+				var face_vertices = [ obj_prim.faces[face_index + 0], obj_prim.faces[face_index + 1], obj_prim.faces[face_index + 2] ];
+				for(var j = 0; j < face_vertices.length; ++j)
 				{
-					name           : obj_prim.name,
-					vertex_buffers : model_prim_vertex_buffers
-				};
+					var face_vertex = face_vertices[j].split("/");
 
-				if(Engine.Util.IsDefined(obj_prim.material))
-				{
-					var material_name = obj_prim.material;
+					// Extract vertex attributes
+					var pos_index    = (face_vertex.length >= 1 && face_vertex[0] != "")? ((face_vertex[0] - 1) * 3) : null;
+					var uv_index     = (face_vertex.length >= 2 && face_vertex[1] != "")? ((face_vertex[1] - 1) * 2) : null;
+					var normal_index = (face_vertex.length >= 3 && face_vertex[2] != "")? ((face_vertex[2] - 1) * 3) : null;
 
-					// For now, add a dummy material to the model (REMOVE THIS!!)
-					if(!Engine.Util.IsDefined(model_file.materials[material_name]))
+					// Add vertex position to enginejs model stream?
+					if(pos_index != null)
 					{
-						model_file.materials[material_name] = dummy_materials[dummy_material_index]
-						dummy_material_index = (dummy_material_index + 1) % dummy_materials.length;
+						prim_vertices.push(obj_file_vertices[pos_index + 0], obj_file_vertices[pos_index + 1], obj_file_vertices[pos_index + 2]);
 					}
 
-					model_prim.material = material_name;
-				}
-
-				model_prims.push(model_prim);
-
-				// Setup streams for model buffers
-				// Note: The faces specified in the obj file are not directly compatible with webgl index buffers as they reference mixed length
-				//       streams. For example the obj might have 10 vertices, 4 uvs, 4 normals and 20 faces. For this reason we
-				//       fully expand all streams and don't use an index buffer. Maybe this could be smarter and figure out whether or not
-				//       it's worth trying to assemble an index buffer for the expanded data!
-				var prim_vertices = [];
-				var prim_uvs = [];
-				var prim_normals = [];
-
-				// For each obj file face...
-				for(var face_index = 0; face_index < obj_prim.faces.length; face_index +=3)
-				{
-					// For each of the 3 vertices on this this face...
-					var face_vertices = [ obj_prim.faces[face_index + 0], obj_prim.faces[face_index + 1], obj_prim.faces[face_index + 2] ];
-					for(var j = 0; j < face_vertices.length; ++j)
+					// Add texture co-ordinate to enginejs model stream?
+					if(uv_index != null)
 					{
-						var face_vertex = face_vertices[j].split("/");
-
-						// Extract vertex attributes
-						var pos_index    = (face_vertex.length >= 1 && face_vertex[0] != "")? ((face_vertex[0] - 1) * 3) : null;
-						var uv_index     = (face_vertex.length >= 2 && face_vertex[1] != "")? ((face_vertex[1] - 1) * 2) : null;
-						var normal_index = (face_vertex.length >= 3 && face_vertex[2] != "")? ((face_vertex[2] - 1) * 3) : null;
-
-						// Add vertex position to enginejs model stream?
-						if(pos_index != null)
-						{
-							prim_vertices.push(obj_file_vertices[pos_index + 0], obj_file_vertices[pos_index + 1], obj_file_vertices[pos_index + 2]);
-						}
-
-						// Add texture co-ordinate to enginejs model stream?
-						if(uv_index != null)
-						{
-							prim_uvs.push(obj_file_uvs[uv_index + 0], 1.0 - obj_file_uvs[uv_index + 1]);
-						}
-
-						// Add normal to enginejs model stream?
-						if(normal_index != null)
-						{
-							prim_normals.push(obj_file_normals[normal_index + 0], obj_file_normals[normal_index + 1], obj_file_normals[normal_index + 2]);
-						}
+						prim_uvs.push(obj_file_uvs[uv_index + 0], 1.0 - obj_file_uvs[uv_index + 1]);
 					}
-				}
 
-				// Generate enginejs vertex buffers from our expanded streams
-				if(prim_vertices.length > 0)
-				{
-					model_prim_vertex_buffers.push(
+					// Add normal to enginejs model stream?
+					if(normal_index != null)
 					{
-						name           : "vertices",
-						attribute_name : "a_pos",
-						item_size      : 3,
-						draw_mode      : "triangles",
-						stream         : prim_vertices
-					});
-				}
-
-				if(prim_uvs.length > 0)
-				{
-					model_prim_vertex_buffers.push(
-					{
-						name           : "texture-coordinates",
-						attribute_name : "a_uv",
-						item_size      : 2,
-						draw_mode      : "triangles",
-						stream         : prim_uvs
-					});
-				}
-
-				if(prim_normals.length > 0)
-				{
-					model_prim_vertex_buffers.push(
-					{
-						name           : "normals",
-						attribute_name : "a_normal",
-						item_size      : 3,
-						draw_mode      : "triangles",
-						stream         : prim_normals
-					});
+						prim_normals.push(obj_file_normals[normal_index + 0], obj_file_normals[normal_index + 1], obj_file_normals[normal_index + 2]);
+					}
 				}
 			}
 
-			// Prepare and finalise model
-			var model_object = Engine.Model.PrepareModel(model_file);
-			callback(model_object);
+			// Generate enginejs vertex buffers from our expanded streams
+			if(prim_vertices.length > 0)
+			{
+				model_prim_vertex_buffers.push(
+				{
+					name           : "vertices",
+					attribute_name : "a_pos",
+					item_size      : 3,
+					draw_mode      : "triangles",
+					stream         : prim_vertices
+				});
+			}
+
+			if(prim_uvs.length > 0)
+			{
+				model_prim_vertex_buffers.push(
+				{
+					name           : "texture-coordinates",
+					attribute_name : "a_uv",
+					item_size      : 2,
+					draw_mode      : "triangles",
+					stream         : prim_uvs
+				});
+			}
+
+			if(prim_normals.length > 0)
+			{
+				model_prim_vertex_buffers.push(
+				{
+					name           : "normals",
+					attribute_name : "a_normal",
+					item_size      : 3,
+					draw_mode      : "triangles",
+					stream         : prim_normals
+				});
+			}
+		}
+
+		// Prepare and finalise model
+		return Engine.Model.PrepareModel(model_file);
+	},
+
+	Load : function(descriptor, callback)
+	{
+		// Go grab the .obj file
+		Engine.Net.FetchResource(descriptor.file, function(obj_file_data)
+		{
+			// Trim excess whitespace and split into lines
+			obj_file_data = obj_file_data.replace(/ +(?= )/g, '');
+			var lines = obj_file_data.split("\n");
+
+			// Does this .obj model reference an external materials file?
+			var materials_file = "";
+			for(var i = 0; i < lines.length; ++i)
+			{
+				var line = lines[i];
+				if(line.substr(0, 6) == "mtllib")
+				{
+					var model_dir = descriptor.file.substring(0, descriptor.file.lastIndexOf("/") + 1);
+					materials_file = model_dir + line.split(" ")[1];
+					break;
+				}
+			}
+
+			// Load the model (called below)
+			var load_model = function(loaded_obj_materials)
+			{
+				// Load the model, passing the loaded materials
+				var model_object = Engine.Model.Importers.OBJ.LoadModelFile(descriptor, lines, loaded_obj_materials);
+				callback(model_object);
+			};
+
+			if(materials_file == "")
+			{
+				// 1. Load the model immediately (with no materials)
+				load_model([]);
+			}
+			else
+			{
+				// 1. Load the materials file first
+				Engine.Model.Importers.OBJ.LoadMaterialsFile(materials_file, function(obj_materials)
+				{
+					// 2. Then load the model, (passing the loaded materials)
+					load_model(obj_materials);
+				});
+			}
 		});
 	},
 };
