f11d8b7ad8c4020c12d155e87b833fd4090e3669
Thomas Sampson <drummertom999@gmail.com>
2017-06-28 16:31:37 +0100

    Added Engine.Colour.Random

--- modules/enginejs-colour.js
+++ modules/enginejs-colour.js
@@ -11,5 +11,10 @@ Engine.Colour =
 	DarkGreen : [0.0,  0.4,  0.0,  1.0],
 	Blue      : [0.0,  0.0,  1.0,  1.0],
 	Orange    : [1.0,  0.6,  0.0,  1.0],
-	Pink      : [0.95, 0.0,  1.0,  1.0]
+	Pink      : [0.95, 0.0,  1.0,  1.0],
+
+	Random    : function()
+	{
+		return [ Math.random(), Math.random(), Math.random(), 1 ];
+	}
 };
