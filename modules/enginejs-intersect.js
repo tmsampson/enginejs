88971b2a486448d490438b4988cd6209c571552e
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2017-12-11 13:48:37 +0000

    Implemented Engine.Intersect.RayPlane

--- modules/enginejs-intersect.js
+++ modules/enginejs-intersect.js
@@ -18,5 +18,29 @@ Engine.Intersect =
 		var radii_squared_sum = (a.radius + b.radius) * (a.radius + b.radius);
 		var squared_dist = Engine.Vec2.DistanceSquared(a.position, b.position);
 		return squared_dist <= radii_squared_sum;
+	},
+
+	RayPlane : function(hit_pos, ray_origin, ray_dir, plane_normal, plane_dist)
+	{
+		var denom = Engine.Vec3.DotProduct(ray_dir, plane_normal);
+		if (denom !== 0)
+		{
+			var t = -(Engine.Vec3.DotProduct(ray_origin, plane_normal) + plane_dist) / denom
+			if (t < 0)
+			{
+				return null;
+			}
+
+			var offset = Engine.Vec3.MultiplyScalar(ray_dir, t);
+			return Engine.Vec3.Add(ray_origin, offset);
+		}
+		else if (Engine.Vec3.DotProduct(plane_normal, ray_origin) + plane_dist === 0)
+		{
+			return Engine.Vec3.Copy(ray_origin);
+		}
+		else
+		{
+			return null;
+		}
 	}
 };
