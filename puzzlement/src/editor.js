5f480a06a23c3b50d526b7a6a8090debed7c1fe2
Thomas Sampson <drummertom999@gmail.com>
2017-12-11 11:06:04 +0000

    Added editor module

---
+++ puzzlement/src/editor.js
@@ -0,0 +1,19 @@
+Editor =
+{
+	GetDebugFloorTileMat : function()
+	{
+		return Core.Resources["mat_floor_debug"];
+	},
+
+	Init : function()
+	{
+	},
+
+	Update : function()
+	{
+	},
+
+	Render : function()
+	{
+	},
+};
