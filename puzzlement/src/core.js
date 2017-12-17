ac7ea731943e74fbc3596a25cf11cac6866f5360
Thomas Sampson <drummertom999@gmail.com>
2017-12-17 20:15:32 +0000

    Removed comment

--- puzzlement/src/core.js
+++ puzzlement/src/core.js
@@ -25,7 +25,6 @@ Core =
 		SkyColour				: [0.5, 0.5, 0.2],
 		Sun :
 		{
-			//angle				: 0, // 0 = straight down y-axis
 			direction			: [ 0, -1, 0 ],
 			ambient				: [ 0.4, 0.4, 0.4 ],
 			colour				: [ 0.4, 0.4, 0.4 ],
