48ee46d976b95d658b21ce11cf64444fcd9f3246
Thomas Sampson <drummertom999@gmail.com>
2018-02-05 14:04:12 +0000

    Sun light position can now be controlled using U/D/L/R/U/J keys in edit mode

--- puzzlement/src/editor/editor-mode-lighting.js
+++ puzzlement/src/editor/editor-mode-lighting.js
@@ -2,6 +2,7 @@ Editor.Mode_Lighting = function()
 {
 	this.Name 					= "LIGHTING";
 	this.EnableCursor 			= false;
+	this.MoveSpeed				= 0.1;
 
 	this.Init = function()
 	{
@@ -15,18 +16,34 @@ Editor.Mode_Lighting = function()
 
 	this.UpdateLight = function()
 	{
-		// Update directional light angle
-		Core.Map.Sun.angle -= Engine.Mouse.GetWheelDelta() / 50;
-		Core.Map.Sun.angle = Engine.Math.Clamp(Core.Map.Sun.angle, -90, 90);
-
-		// Update directional light position
-		Core.Map.Sun.position[0] = Math.sin(Engine.Math.DegToRad(Core.Map.Sun.angle)) * Core.Map.Sun.arc_radius[0];
-		Core.Map.Sun.position[1] = Core.Map.Sun.arc_lift + Math.cos(Engine.Math.DegToRad(Core.Map.Sun.angle)) * Core.Map.Sun.arc_radius[1];
-		Core.Map.Sun.position[2] = 8;
+		// Move light around
+		if(Engine.Keyboard.WasJustPressed("left"))
+		{
+			Core.Map.Sun.position[0] -= this.MoveSpeed;
+		}
+		if(Engine.Keyboard.WasJustPressed("right"))
+		{
+			Core.Map.Sun.position[0] += this.MoveSpeed;
+		}
+		if(Engine.Keyboard.WasJustPressed("up"))
+		{
+			Core.Map.Sun.position[2] += this.MoveSpeed;
+		}
+		if(Engine.Keyboard.WasJustPressed("down"))
+		{
+			Core.Map.Sun.position[2] -= this.MoveSpeed;
+		}
+		if(Engine.Keyboard.WasJustPressed("u"))
+		{
+			Core.Map.Sun.position[1] += this.MoveSpeed;
+		}
+		if(Engine.Keyboard.WasJustPressed("j"))
+		{
+			Core.Map.Sun.position[1] -= this.MoveSpeed;
+		}
 
 		// Update directional light direction
-		var len = Math.sqrt((Core.Map.Sun.position[0] * Core.Map.Sun.position[0]) + (Core.Map.Sun.position[1]  * Core.Map.Sun.position[1]));
-		Core.Map.Sun.direction = [ -Core.Map.Sun.position[0] / len, -Core.Map.Sun.position[1] / len, 0 ];
+		Core.Map.Sun.direction = Engine.Vec3.Normalise(Engine.Vec3.MultiplyScalar(Core.Map.Sun.position, -1.0));
 		Engine.Gfx.SetDirectionalLight(Core.Map.Sun);
 	};
 
