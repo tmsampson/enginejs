f4b12f2bbb21ec3df8ffce42c3a40fa62b60fc6b
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2018-02-06 16:20:26 +0000

    * Player movement now framerate independent * Player can now be controlled with gamepad

--- puzzlement/src/player.js
+++ puzzlement/src/player.js
@@ -1,15 +1,19 @@
 Core.Player = function()
 {
-	// Config
-	this.WalkSpeed = 0.06;
-	this.RunSpeed = 0.12;
+	// Player config
+	this.WalkSpeed = 1.5;
+	this.RunSpeed = 3;
 	this.HeadHeight = 1;
 	this.HeadBobHeight = 0.01;
 	this.HeadBobSpeed = 12;
 	this.HeadBobHeightRunning = 0.015;
 	this.HeadBobSpeedRunning = 16;
-	this.LookSpeed = 1.5;
-	this.LookInvertY = true;
+
+	// Input config
+	this.MouseLookSensitivity = [ 2.5, 2.5 ];
+	this.MouseInvertY = false;
+	this.GamepadLookSensitivity = [ 120, 120 ];
+	this.GamepadInvertY = true;
 
 	// Transform
 	this.Position = [0, this.HeadHeight, 0];
@@ -32,42 +36,48 @@ Core.Player = function()
 
 	this.ProcessInput = function()
 	{
-		var move_attempted = false;
-		var is_running = Engine.Keyboard.IsPressed("shift");
-		var move_speed = is_running? this.RunSpeed : this.WalkSpeed;
+		var gamepad = Engine.Gamepad.Pads[0];
+		var is_running = Engine.Keyboard.IsPressed("shift") || (gamepad && gamepad.IsPressed("lt"));
+		var move_speed = (is_running? this.RunSpeed : this.WalkSpeed) * Engine.Time.delta_s;
 		var movement_delta = [0, 0, 0];
 
 		// Strafe forwards
 		if(Engine.Keyboard.IsPressed("w"))
 		{
 			movement_delta = Engine.Vec3.MultiplyScalar(this.Forward, move_speed);
-			move_attempted = true;
 		}
 
 		// Strafe backwards
 		if(Engine.Keyboard.IsPressed("s"))
 		{
 			movement_delta = Engine.Vec3.MultiplyScalar(this.Forward, -move_speed);
-			move_attempted = true;
 		}
 
 		// Strafe left
 		if(Engine.Keyboard.IsPressed("a"))
 		{
 			movement_delta = Engine.Vec3.MultiplyScalar(this.Right, -move_speed);
-			move_attempted = true;
 		}
 
 		// Strafe right
 		if(Engine.Keyboard.IsPressed("d"))
 		{
 			movement_delta = Engine.Vec3.MultiplyScalar(this.Right, move_speed);
-			move_attempted = true;
+		}
+
+		// Gamepad strafe
+		if(gamepad)
+		{
+			var left_stick = gamepad.GetLeftStick();
+			var right = Engine.Vec3.MultiplyScalar(this.Right, move_speed * left_stick[0]);
+			var forward = Engine.Vec3.MultiplyScalar(this.Forward, move_speed * left_stick[1]);
+			var strafe = Engine.Vec3.Add(right, forward);
+			movement_delta = Engine.Vec3.Add(movement_delta, strafe);
 		}
 
 		// Apply movement?
 		var has_moved = false;
-		if(move_attempted)
+		if(Engine.Vec3.Length(movement_delta) > 0.0)
 		{
 			var destination = Engine.Vec3.Add(this.Position, movement_delta);
 			if(Collision.PositionIsValid(destination, this.CollisionRadius))
@@ -77,13 +87,33 @@ Core.Player = function()
 			}
 		}
 
-		// Look?
+		// Query look
+		var look_delta = [0, 0];
 		if(Engine.Mouse.IsPressed("right"))
 		{
-			var delta = Engine.Mouse.GetDelta();
-			var rot_y = Engine.Math.DegToRad(-delta[0] * this.LookSpeed * Engine.Time.delta_s);
-			mat4.rotate(this.ScratchMatrix, Engine.Math.IdentityMatrix, rot_y, [0, 1, 0]);
-			mat4.rotate(this.ScratchMatrix, this.ScratchMatrix, Engine.Math.DegToRad(delta[1] * this.LookSpeed * Engine.Time.delta_s), this.Right);
+			look_delta = Engine.Vec2.Multiply(Engine.Mouse.GetDelta(), this.MouseLookSensitivity);
+			if(this.MouseInvertY)
+			{
+				look_delta[1] *= -1;
+			}
+			Engine.Log(look_delta);
+		}
+		else if(gamepad)
+		{
+			look_delta = Engine.Vec2.Multiply(gamepad.GetRightStick(), this.GamepadLookSensitivity);
+			if(this.GamepadInvertY)
+			{
+				look_delta[1] *= -1;
+			}
+		}
+
+		// Apply look?
+		if(Engine.Vec2.Length(look_delta) > 0.0)
+		{
+			var rot_up = Engine.Math.DegToRad(-look_delta[0]) * Engine.Time.delta_s;
+			var rot_right = Engine.Math.DegToRad(look_delta[1]) * Engine.Time.delta_s;
+			mat4.rotate(this.ScratchMatrix, Engine.Math.IdentityMatrix, rot_up, this.Up);
+			mat4.rotate(this.ScratchMatrix, this.ScratchMatrix, rot_right, this.Right);
 			vec3.transformMat4(this.Forward, this.Forward, this.ScratchMatrix);
 		}
 
